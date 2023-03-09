a06d3a7a6567967a1d4e110b0706e9631aa57c52
Cem Yilmaz <cem.yilmaz@sap.com>
2023-03-09 21:53:02 +0100

    initial commit 🚀

---
+++ dist/server.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const url_1 = require("url");
+const next_1 = __importDefault(require("next"));
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const app = (0, next_1.default)({ dev });
+const handle = app.getRequestHandler();
+app.prepare().then(() => {
+    (0, http_1.createServer)((req, res) => {
+        const parsedUrl = (0, url_1.parse)(req.url, true);
+        handle(req, res, parsedUrl);
+    }).listen(port);
+    console.log(`> Server listening at http://localhost:${port} as ${dev ? 'development' : process.env.NODE_ENV}`);
+});
