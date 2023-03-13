e83b0f7d8bd17ad90b05d6caa42b202238d86fcd
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-13 21:02:15 +0100

    change PeopleEat to hello world

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Hello PeopleEat
+            Hello world!
           </p>
           <div>
             <a
