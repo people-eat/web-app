5dfe89919715c4ef35528174d5511e88604f507a
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-19 17:12:56 +0100

    remove old setup

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  console.log('hello world!');
-  console.log('Some changes!');
-
-  return <Component {...pageProps} />
-}
