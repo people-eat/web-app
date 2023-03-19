08e578af6036c5c3c83c59a2870fc3809069206e
Llavrov <levlevlavrov@gmail.com>
2023-03-19 23:02:10 +0300

    fix eslint

--- apps/web-app-e2e-e2e/cypress.config.ts
+++
@@ -1,6 +0,0 @@
-import { nxE2EStorybookPreset } from '@nrwl/storybook/presets/cypress';
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
-    e2e: nxE2EStorybookPreset(__dirname),
-});
