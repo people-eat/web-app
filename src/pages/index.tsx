5697e3eddd309536c9bcc1f153c43057d2799845
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-26 00:57:49 +0100

    initial commit 🚀

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>PeopleEat</title>
+        <meta name="description" content="PeopleEat - a platform to find private chefs / cooks" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
