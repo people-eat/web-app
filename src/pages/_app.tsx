5794a2f8d097de68bfed96a3b2ce68a513bdce6b
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-26 00:57:49 +0100

    chore: initial commit 🚀

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { type AppType } from "next/dist/shared/lib/utils";
+
+import "~/styles/globals.css";
+
+const PeopleEatApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default PeopleEatApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>PeopleEat</title>
+        <meta name="description" content="PeopleEat - a platform to find private chefs / cooks" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Index;
