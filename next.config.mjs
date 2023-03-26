1fefe3867a836b511d8439da69ad48a533164eeb
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-26 01:07:12 +0100

    chore: setup formatting with prettier

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 
-  pageExtensions: ['tsx'],
+    pageExtensions: ['tsx'],
 
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+    /**
+     * If you have the "experimental: { appDir: true }" setting enabled, then you
+     * must comment the below `i18n` config out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default config;
