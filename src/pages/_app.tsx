f48cc2861904ba7e5f11e65fd921950f2bc2fd17
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-26 01:40:13 +0100

    add material ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
+import { ThemeProvider, createTheme, type Theme } from '@mui/material';
 import { type AppType } from 'next/dist/shared/lib/utils';
 
 import '~/styles/globals.css';
 
 const PeopleEatApp: AppType = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+    const theme: Theme = createTheme({
+        palette: {
+            primary: {
+                main: '#FF6433',
+            },
+        },
+    });
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 };
 
 export default PeopleEatApp;
