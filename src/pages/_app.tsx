34d9d0342e068c7472341f82087edd07b572d88e
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-26 03:09:57 +0200

    setup apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import { ApolloProvider } from '@apollo/client';
 import { type AppType } from 'next/dist/shared/lib/utils';
+import apolloClient from '../data-source/apolloClient';
 
 import '~/styles/globals.css';
 
 const PeopleEatApp: AppType = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+    return (
+        <ApolloProvider client={apolloClient}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    );
 };
 
 export default PeopleEatApp;
