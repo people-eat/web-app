c1551b355f4d6ed29de4aa0b9e910a86bf6f9d02
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-26 01:30:13 +0100

    setup linting with eslint

--- src/components/standard/PeopleEatButton.tsx
+++ src/components/standard/PeopleEatButton.tsx
@@ -1,3 +1,5 @@
-export default function PeopleEatButton() {
+import { type ReactElement } from 'react';
+
+export default function PeopleEatButton(): ReactElement {
     return <button style={{ backgroundColor: 'orange', padding: 8 }}>Click me</button>;
 }
