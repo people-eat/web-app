6f31a1dd023cc2a0846ccf6e765780fae54e76be
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-26 03:25:28 +0200

    chore: setup for custom hosting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,7 @@ const config = {
             },
         },
     ],
-    ignorePatterns: ['next-env.d.ts', 'src/data-source/generated/*'],
+    ignorePatterns: ['next-env.d.ts', 'src/data-source/generated/*', 'server.js'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         project: path.join(__dirname, 'tsconfig.json'),
