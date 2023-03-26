989eeee0ca806134c06ef78319b495719e3e51ba
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-26 01:07:12 +0100

    setup formatting with prettier

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,17 +1,17 @@
-import { type NextPage } from "next";
-import Head from "next/head";
+import { type NextPage } from 'next';
+import Head from 'next/head';
 
 const SignUpPage: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>PeopleEat - Sign Up</title>
-        <meta name="description" content="PeopleEat - Sign Up" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>Sign Up</main>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>PeopleEat - Sign Up</title>
+                <meta name="description" content="PeopleEat - Sign Up" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>Sign Up</main>
+        </>
+    );
 };
 
 export default SignUpPage;
