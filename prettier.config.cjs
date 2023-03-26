15a2301fc399aa5332e63e780afe7dc598588464
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-03-26 01:07:12 +0100

    setup formatting with prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,18 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+    plugins: [require.resolve('prettier-plugin-tailwindcss')],
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    printWidth: 140,
+    overrides: [
+        {
+            files: ['*.yaml', '*.yml'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
 };
 
 module.exports = config;
