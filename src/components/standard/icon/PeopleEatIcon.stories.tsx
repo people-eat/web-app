8436854adb7ee72bf9bce81b8f9c7d438154a07e
Llavrov <levlevlavrov@gmail.com>
2023-03-29 17:14:28 +0300

    add PeopleEatIcon component

---
+++ src/components/standard/icon/PeopleEatIcon.stories.tsx
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Icon } from './Icon';
+import PeopleEatIcon from './PeopleEatIcon';
+
+const meta: Meta<typeof PeopleEatIcon> = {
+    title: 'Standard Components/PeopleEatIcon',
+    component: PeopleEatIcon,
+};
+
+export default meta;
+
+export const Apple: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.apple } };
+
+export const ArrowNarrowRight: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.arrowNarrowRight } };
+
+export const ArrowRight: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.arrowRight } };
+
+export const ArrowReload: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.arrowReload } };
+
+export const BarChart: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.barChart } };
+
+export const Camera: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.camera } };
+
+export const ChatDots: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.chatDots } };
+
+export const Check: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.check } };
+
+export const CheckWhite: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.checkWhite } };
+
+export const Close: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.close } };
+
+export const CoinsHand: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.coinsHand } };
+
+export const CreditCard: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.creditCard } };
+
+export const Data: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.data } };
+
+export const Download: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.download } };
+
+export const EditPencil: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.editPencil } };
+
+export const Eye: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.eye } };
+
+export const EyeCrossedOut: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.eyeCrossedOut } };
+
+export const Facebook: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.facebook } };
+
+export const File: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.file } };
+
+export const FileSearch: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.fileSearch } };
+
+export const FilterLines: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.filterLines } };
+
+export const Folder: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.folder } };
+
+export const Forward: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.forward } };
+
+export const Google: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.google } };
+
+export const Heart: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.heart } };
+
+export const HeartFull: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.heartFull } };
+
+export const Home: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.home } };
+
+export const List: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.list } };
+
+export const MarkerPin: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.markerPin } };
+
+export const MessageChat: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.messageChat } };
+
+export const Minus: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.minus } };
+
+export const MoreApps: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.moreApps } };
+
+export const Navigation: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.navigation } };
+
+export const Play: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.play } };
+
+export const Plus: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.plus } };
+
+export const ProfileEmpty: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.profileEmpty } };
+
+export const Search: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.search } };
+
+export const Settings: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.settings } };
+
+export const ShieldCheck: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.shieldCheck } };
+
+export const Star: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.star } };
+
+export const UserCardId: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.userCardId } };
+
+export const Users: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.users } };
+
+export const UsersOrange: StoryObj<typeof PeopleEatIcon> = { args: { icon: Icon.usersOrange } };
