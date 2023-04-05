8620b924773cca55cbb72b84e15b18322f6fb4d2
Llavrov <levlevlavrov@gmail.com>
2023-04-05 19:30:10 +0300

    replace imports

--- src/components/standard/buttons/PeopleEatNext.tsx
+++ src/components/standard/buttons/PeopleEatNext.tsx
@@ -1,6 +1,7 @@
 import IconButton from '@mui/material/Button';
 import { type ReactElement } from 'react';
-import { IconsList, PeopleEatIcon } from '~/components/standard/icon/PeopleEatIcon';
+import { Icon } from '~/components/standard/icon/Icon';
+import PeopleEatIcon from '~/components/standard/icon/PeopleEatIcon';
 
 export default function PeopleEatNext(): ReactElement {
     return (
@@ -10,7 +11,7 @@ export default function PeopleEatNext(): ReactElement {
             variant="contained"
             className="min-w-[46px] hover:bg-orange p-0 border-border hover:border-transparent"
         >
-            <PeopleEatIcon icon={IconsList.arrowRight} Classes={'hover:invert'} height={46} width={46} />
+            <PeopleEatIcon icon={Icon.arrowRight} Classes={'hover:invert'} height={46} width={46} />
         </IconButton>
     );
 }
