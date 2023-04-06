a98396712446726a099af30d96cade8617bd2111
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-06 23:21:45 +0200

    add PeopleEatTabItem

---
+++ src/components/standard/tabItem/PeopleEatTabItem.tsx
@@ -0,0 +1,20 @@
+import Button from '@mui/material/Button';
+import classNames from 'classnames';
+import { useState, type ReactElement } from 'react';
+import { type PeopleEatTabItemProps } from './PeopleEatTabItemProps';
+
+export default function PeopleEatTabItem({ disabled = false, title }: PeopleEatTabItemProps): ReactElement {
+    const [selected, setSelect] = useState(false);
+
+    return (
+        <Button
+            className={classNames('text-black hover:text-white hover:bg-hover pl-5 pr-5 pt-2 pb-2 rounded-5', {
+                ['bg-orange text-white']: selected,
+            })}
+            disabled={Boolean(disabled)}
+            onClick={(): void => setSelect(!selected)}
+        >
+            {title}
+        </Button>
+    );
+}
