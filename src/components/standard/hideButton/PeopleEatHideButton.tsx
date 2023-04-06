04ec24e67435b5de7d56388db55193c422963bbf
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-06 23:22:46 +0200

    add PeopleEatHideButton

---
+++ src/components/standard/hideButton/PeopleEatHideButton.tsx
@@ -0,0 +1,38 @@
+import { styled } from '@mui/material';
+import Checkbox from '@mui/material/Checkbox';
+import { type ReactElement } from 'react';
+import { type PeopleEatHideButtonProps } from './PeopleEatHideButtonProps';
+
+export default function PeopleEatHideButton({ disabled = false, defaultChecked = true, onClick }: PeopleEatHideButtonProps): ReactElement {
+    const BpIcon = styled('span')(() => ({
+        borderRadius: 5,
+        width: 24,
+        height: 24,
+        boxShadow: 'boxShadow: 0 0 0 1px transparent',
+        backgroundImage: 'url(/icons/eye-crossed-out.svg)',
+    }));
+
+    const BpCheckedIcon = styled(BpIcon)({
+        backgroundImage: 'url(/icons/eye.svg)',
+        content: '""',
+        boxShadow: '0 0 0 1px transparent',
+        '&:before': {
+            display: 'block',
+            width: 24,
+            height: 24,
+            content: '""',
+        },
+    });
+
+    return (
+        <div>
+            <Checkbox
+                onClick={onClick}
+                icon={<BpIcon />}
+                checkedIcon={<BpCheckedIcon />}
+                defaultChecked={Boolean(defaultChecked)}
+                disabled={disabled}
+            />
+        </div>
+    );
+}
