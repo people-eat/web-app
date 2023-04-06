2ddd8a3a835f8944a9aaddf4d64fbdb3dbdb5dfb
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-07 00:38:07 +0200

    setup storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-dark-mode'],
+    framework: {
+        name: '@storybook/nextjs',
+        options: {},
+    },
+    docs: {
+        autodocs: true,
+    },
+    staticDirs: ['../public'],
+};
+
+export default config;
