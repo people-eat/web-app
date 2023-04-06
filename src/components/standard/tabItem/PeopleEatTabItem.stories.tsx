2875fd776c25d3031f8fd7fe8090f6baf9376883
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-06 23:21:45 +0200

    add PeopleEatTabItem

---
+++ src/components/standard/tabItem/PeopleEatTabItem.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PeopleEatTabItem from './PeopleEatTabItem';
+
+const meta: Meta<typeof PeopleEatTabItem> = {
+    title: 'Standard Components/PeopleEatTabItem',
+    component: PeopleEatTabItem,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PeopleEatTabItem> = {
+    args: {
+        title: 'European',
+    },
+};
+
+export const ActiveComponent: StoryObj<typeof PeopleEatTabItem> = {
+    args: {
+        active: true,
+        title: 'European',
+    },
+};
