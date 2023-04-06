0ec6c108848c5ccdd42628d730f453d8c8f26686
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-06 23:21:45 +0200

    feat: add PETabItem

---
+++ src/components/standard/tabItem/PETabItem.tsx
@@ -0,0 +1,26 @@
+import Button from '@mui/material/Button';
+import { type ReactElement } from 'react';
+import { type PETabItemProps } from './PETabItemProps';
+
+export default function PETabItem({ disabled = false, title, active, onClick }: PETabItemProps): ReactElement {
+    return (
+        <section>
+            <Button
+                sx={{
+                    borderRadius: '20px',
+                    color: active ? 'white' : 'black',
+                    padding: '8px 20px',
+                    '&:hover': {
+                        color: 'white',
+                        backgroundColor: 'rgba(255, 100, 51, .7)',
+                    },
+                    backgroundColor: active ? 'rgba(255, 100, 51, 1)' : 'rgba(245, 245, 245, 0.5)',
+                }}
+                disabled={Boolean(disabled)}
+                onClick={onClick}
+            >
+                {title}
+            </Button>
+        </section>
+    );
+}
