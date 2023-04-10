58eeab5324a6783ff378afcca70e7d7152c0d0c6
Llavrov <levlevlavrov@gmail.com>
2023-04-10 16:19:23 +0300

    add PeopleEatChoice and tailwind setup

--- src/components/standard/tabItem/PeopleEatTabItem.tsx
+++ src/components/standard/tabItem/PeopleEatTabItem.tsx
@@ -7,14 +7,16 @@ export default function PeopleEatTabItem({ disabled = false, title, active }: Pe
     const [selected, setSelect] = useState(active);
 
     return (
-        <Button
-            className={classNames('text-black hover:text-white hover:bg-hover pl-5 pr-5 pt-2 pb-2 rounded-5', {
-                ['bg-orange text-white']: selected,
-            })}
-            disabled={Boolean(disabled)}
-            onClick={(): void => setSelect(!selected)}
-        >
-            {title}
-        </Button>
+        <section>
+            <Button
+                className={classNames('text-black hover:text-white hover:bg-hover pl-5 pr-5 pt-2 pb-2 rounded-5', {
+                    ['bg-orange text-white']: selected,
+                })}
+                disabled={Boolean(disabled)}
+                onClick={(): void => setSelect(!selected)}
+            >
+                {title}
+            </Button>
+        </section>
     );
 }
