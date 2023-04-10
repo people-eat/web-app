497fb4dd9164aa0c5c1b43847182bdf0fdec82a1
Llavrov <levlevlavrov@gmail.com>
2023-04-10 17:59:29 +0300

    PeopleEatChoice component and fix imports

--- src/components/standard/choice/PeopleEatChoice.tsx
+++ src/components/standard/choice/PeopleEatChoice.tsx
@@ -1,6 +1,6 @@
 import { type ReactElement } from 'react';
-import { Icon } from '~/components/standard/icon/Icon';
-import PeopleEatIcon from '~/components/standard/icon/PeopleEatIcon';
+import { Icon } from '../icon/Icon';
+import PeopleEatIcon from '../icon/PeopleEatIcon';
 
 interface IPeopleEatChoiceProps {
     title: string;
@@ -12,7 +12,9 @@ export default function PeopleEatChoice({ title, onClose }: IPeopleEatChoiceProp
         <div className="relative text-text-m-bold px-4 py-2 bg-base rounded-5 max-w-[130px]">
             <span className="flex items-center flex-row gap-4">
                 {title}
-                <PeopleEatIcon icon={Icon.close} className={'cursor-pointer flex items-center'} onClick={onClose} edgeLength={24} />
+                <div onClick={onClose}>
+                    <PeopleEatIcon icon={Icon.close} className={'cursor-pointer flex items-center'} edgeLength={24} />
+                </div>
             </span>
         </div>
     );
