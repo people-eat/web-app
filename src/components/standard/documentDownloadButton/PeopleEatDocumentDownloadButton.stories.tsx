6362e6d13d9b8880f55c46081a3c2872c048fa39
Llavrov <levlevlavrov@gmail.com>
2023-04-11 22:30:06 +0300

    stories for PeopleEatDocumentDownloadButton and PeopleEatCreditCard

---
+++ src/components/standard/documentDownloadButton/PeopleEatDocumentDownloadButton.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PeopleEatDocumentDownloadButton from './PeopleEatDocumentDownloadButton';
+
+const meta: Meta<typeof PeopleEatDocumentDownloadButton> = {
+    title: 'Standard Components/PeopleEatDocumentDownloadButton',
+    component: PeopleEatDocumentDownloadButton,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PeopleEatDocumentDownloadButton> = {
+    args: {},
+};
