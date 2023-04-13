072a6262175685a7379a28fe19d646a613d72513
Llavrov <levlevlavrov@gmail.com>
2023-04-13 17:29:51 +0300

    feat: add PEChatCard

---
+++ src/components/cards/chatCard/PEChatCardProps.ts
@@ -0,0 +1,11 @@
+export interface PEChatCardProps {
+    onClick: () => void;
+    label?: string;
+    menuTitle?: string;
+    date?: string;
+    time?: string;
+    place?: string;
+    userName?: string;
+    lastDateActive?: string;
+    imageUrl?: string;
+}
