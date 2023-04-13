a46b05428789da44638d3bc117c4da5ec0a3b0a7
Llavrov <levlevlavrov@gmail.com>
2023-04-13 17:40:24 +0300

    PeopleEatDocumentDownloadButton, PeopleEatEditButton, PeopleEatHideButton

--- src/components/standard/editButton/PeopleEatEditButton.tsx
+++ src/components/standard/editButton/PeopleEatEditButton.tsx
@@ -1,6 +1,7 @@
 import { type ReactElement } from 'react';
 import { Icon } from '../icon/Icon';
 import PeopleEatIcon from '../icon/PeopleEatIcon';
+import IconButton from "@mui/material/Button";
 
 interface IPeopleEatEditButtonProps {
     onClick: () => void;
@@ -8,8 +9,26 @@ interface IPeopleEatEditButtonProps {
 
 export default function PeopleEatEditButton({ onClick }: IPeopleEatEditButtonProps): ReactElement {
     return (
-        <section onClick={onClick} className={'flex justify-center items-center bg-base w-10 h-10 rounded-3'}>
+        <IconButton
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: '12px',
+                height: '40px',
+                width: '40px',
+                minWidth: '40px',
+                '&:hover': {
+                    backgroundColor: 'rgba(245, 245, 245, 1)',
+                },
+                backgroundColor: 'rgba(245, 245, 245, 1)',
+                padding: 0,
+            }}
+            size={'small'}
+            variant="contained"
+            onClick={onClick}
+        >
             <PeopleEatIcon icon={Icon.editPencil} className={'cursor-pointer'} edgeLength={24} />
-        </section>
+        </IconButton>
     );
 }
