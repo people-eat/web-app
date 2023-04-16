cd88b45ad0ec83912c1d8143550bb250b614c39a
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-16 10:26:55 +0200

    feat: add PEIconButton

---
+++ src/components/standard/iconButton/PEIconButton.tsx
@@ -0,0 +1,53 @@
+import IconButton from '@mui/material/Button';
+import classNames from 'classnames';
+import { type ReactElement } from 'react';
+import { type Icon } from '../icon/Icon';
+import PEIcon from '../icon/PEIcon';
+
+export interface PEIconButtonProps {
+    onClick?: () => void;
+    icon: Icon;
+    withoutShadow?: boolean;
+    size?: string;
+    bg?: string;
+    className?: string;
+    iconSize?: number;
+}
+
+export default function PEIconButton({
+    onClick,
+    bg,
+    withoutShadow,
+    icon,
+    size = '36px',
+    iconSize,
+    className,
+}: PEIconButtonProps): ReactElement {
+    return (
+        <IconButton
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: '12px',
+                height: size,
+                width: size,
+                minWidth: size,
+                boxShadow: withoutShadow ? '0 0 0 rgba(0, 0, 0, 0)' : '',
+                '&:hover': {
+                    backgroundColor: bg ?? 'rgba(245, 245, 245, 1)',
+                },
+                '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+                    boxShadow: withoutShadow ? '0 0 0 rgba(0, 0, 0, 0)' : '',
+                },
+                backgroundColor: bg ?? 'rgba(245, 245, 245, 1)',
+                padding: 0,
+            }}
+            size={'small'}
+            variant="contained"
+            onClick={onClick}
+        >
+            <PEIcon icon={icon} className={classNames('cursor-pointer', className)} edgeLength={iconSize ?? 16} />
+        </IconButton>
+    );
+}
