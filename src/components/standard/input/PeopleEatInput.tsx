f211e33c6ccc8c0da7a4f6a6f607dacfc9bba92e
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-16 10:06:30 +0200

    add PeopleEatInput

---
+++ src/components/standard/input/PeopleEatInput.tsx
@@ -0,0 +1,69 @@
+import { OutlinedInput } from '@mui/material';
+import classNames from 'classnames';
+import { useState, type ChangeEvent, type ReactElement } from 'react';
+import { isEmail } from '../../../utils/isEmail';
+import PeopleEatHideButton from '../../standard/hideButton/PeopleEatHideButton';
+
+interface PeopleEatInputProps {
+    disabled?: boolean;
+    password?: boolean;
+    email?: boolean;
+}
+
+type TInputChangeEvent = ChangeEvent<HTMLTextAreaElement | HTMLInputElement>;
+
+export default function PeopleEatInput({ disabled, password: pass, email }: PeopleEatInputProps): ReactElement {
+    const [isShowPass, setShowPass] = useState(Boolean(pass));
+    const [isError, setError] = useState(false);
+    const [isValid, setIsValid] = useState(false);
+    const [_emailValue, setEmail] = useState({ email: 'your-email@gmail.com', isValidEmail: true });
+    const type = isShowPass ? 'password' : email ? 'email' : 'text';
+
+    function handleEmailChange(value: string, isValidEmail: boolean): void {
+        setEmail({ email: value, isValidEmail: isValidEmail });
+        setError(!isValidEmail);
+    }
+
+    function handleInputChange({ target }: TInputChangeEvent): void {
+        if (email) {
+            setIsValid(isEmail(target.value));
+            handleEmailChange(target.value, isValid);
+        }
+    }
+
+    const decorator = pass ? (
+        <div className={'opacity-50 hover:opacity-100 z-50'}>
+            <PeopleEatHideButton onClick={(): void => setShowPass(!isShowPass)} />
+        </div>
+    ) : email ? (
+        <span
+            className={classNames('text-disabled', {
+                ['text-red-500']: isError,
+            })}
+        >
+            Enter your email
+        </span>
+    ) : null;
+
+    return (
+        <section className={'relative flex justify-center items-center rounded-3 w-full'}>
+            <OutlinedInput
+                error={isError}
+                sx={{
+                    borderBottom: 0,
+                    '&:hover': {
+                        borderBottom: 0,
+                    },
+                    borderRadius: '12px',
+                }}
+                type={type}
+                disabled={Boolean(disabled)}
+                onChange={handleInputChange}
+                fullWidth
+                required
+                placeholder="Type in here..."
+            />
+            <span className={'absolute right-2'}>{decorator}</span>
+        </section>
+    );
+}
