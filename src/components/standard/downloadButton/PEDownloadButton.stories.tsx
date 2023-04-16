82e1c0735fe649bd01511507234cb35ff7480cb2
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-16 10:24:05 +0200

    feat: add PEDownloadButton

---
+++ src/components/standard/downloadButton/PEDownloadButton.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PEDownloadButton from './PEDownloadButton';
+
+const meta: Meta<typeof PEDownloadButton> = {
+    title: 'Standard Components/PEDownloadButton',
+    component: PEDownloadButton,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PEDownloadButton> = {
+    args: {
+        disabled: false,
+        title: 'Invoice',
+        downloadSize: '1.000 MB',
+    },
+};
