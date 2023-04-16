6823d0e7b956209f9e9861b84e15d5516c071745
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-16 10:26:55 +0200

    add PeopleEatCameraButton

---
+++ src/components/standard/cameraButton/PeopleEatCameraButton.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PeopleEatCameraButton from './PeopleEatCameraButton';
+
+const meta: Meta<typeof PeopleEatCameraButton> = {
+    title: 'Standard Components/PeopleEatCameraButton',
+    component: PeopleEatCameraButton,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PeopleEatCameraButton> = {
+    args: {
+        onClick: undefined,
+    },
+};
