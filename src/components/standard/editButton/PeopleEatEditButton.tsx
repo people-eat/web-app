14d6cc7c8b3e1bfbb4dae95728f118f05ad3f6e4
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-16 10:12:07 +0200

    add PeopleEatEditButton

---
+++ src/components/standard/editButton/PeopleEatEditButton.tsx
@@ -0,0 +1,34 @@
+import IconButton from '@mui/material/Button';
+import { type ReactElement } from 'react';
+import { Icon } from '../icon/Icon';
+import PeopleEatIcon from '../icon/PeopleEatIcon';
+
+interface IPeopleEatEditButtonProps {
+    onClick: () => void;
+}
+
+export default function PeopleEatEditButton({ onClick }: IPeopleEatEditButtonProps): ReactElement {
+    return (
+        <IconButton
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: '12px',
+                height: '40px',
+                width: '40px',
+                minWidth: '40px',
+                '&:hover': {
+                    backgroundColor: 'rgba(245, 245, 245, 1)',
+                },
+                backgroundColor: 'rgba(245, 245, 245, 1)',
+                padding: 0,
+            }}
+            size={'small'}
+            variant="contained"
+            onClick={onClick}
+        >
+            <PeopleEatIcon icon={Icon.editPencil} className={'cursor-pointer'} edgeLength={24} />
+        </IconButton>
+    );
+}
