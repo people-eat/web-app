627a6cabccc3d31a9ea7970843a77381a5b87f5c
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-23 20:02:12 +0200

    chore: setup mui date picker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider, createTheme, type Theme } from '@mui/material';
+import { LocalizationProvider as MuiI18nProvider } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import moment from 'moment';
 import { type AppType } from 'next/dist/shared/lib/utils';
+import { useRouter } from 'next/router';
 import createApolloClient from '../data-source/createApolloClient';
 
+import 'moment/locale/de';
+import 'moment/locale/fr';
+import 'moment/locale/ru';
 import '~/styles/globals.css';
 
 export const theme: Theme = createTheme({
@@ -15,10 +22,15 @@ export const theme: Theme = createTheme({
 });
 
 const PeopleEatApp: AppType = ({ Component, pageProps }) => {
+    const { locale } = useRouter();
+    moment.locale(locale);
+
     return (
         <ApolloProvider client={createApolloClient(process.env.NEXT_PUBLIC_SERVER_URL ?? '')}>
             <ThemeProvider theme={theme}>
-                <Component {...pageProps} />
+                <MuiI18nProvider dateAdapter={AdapterMoment} adapterLocale={locale}>
+                    <Component {...pageProps} />
+                </MuiI18nProvider>
             </ThemeProvider>
         </ApolloProvider>
     );
