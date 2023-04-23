5c1ad54e8da418b02cae2a57388512d0fc0f393d
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-23 20:02:12 +0200

    chore: setup mui date picker

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,18 +1,25 @@
 import { ThemeProvider } from '@mui/material';
+import { LocalizationProvider as MuiI18nProvider } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import type { Preview } from '@storybook/react';
+import moment from 'moment';
 import I18nProvider from 'next-translate/I18nProvider';
 import React, { type ReactElement } from 'react';
 import common from '../locales/en/common.json';
+import home from '../locales/en/home.json';
 import { theme } from '../src/pages/_app';
 import '../src/styles/globals.css';
 
 export const decorators = [
     (Story: any): ReactElement => {
+        moment.locale('en');
         return (
             <ThemeProvider theme={theme}>
-                <I18nProvider lang={'en'} namespaces={{ common }}>
-                    <Story />
-                </I18nProvider>
+                <MuiI18nProvider dateAdapter={AdapterMoment} adapterLocale={'en'}>
+                    <I18nProvider lang={'en'} namespaces={{ common, home }}>
+                        <Story />
+                    </I18nProvider>
+                </MuiI18nProvider>
             </ThemeProvider>
         );
     },
