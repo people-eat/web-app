3c080cfb2c1b3faa8aa4a1cdc2ce656a14d02540
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-24 01:35:51 +0200

    feat: add sign up page

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,7 +1,24 @@
+import { Button, Checkbox, FormControlLabel, FormGroup, TextField } from '@mui/material';
 import { type NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { Icon } from '../../components/standard/icon/Icon';
+import PeopleEatIcon from '../../components/standard/icon/PeopleEatIcon';
+import HStack from '../../components/utility/hStack/HStack';
+import Spacer from '../../components/utility/spacer/Spacer';
+import VStack from '../../components/utility/vStack/VStack';
 
 const SignUpPage: NextPage = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordRepeat, setPasswordRepeat] = useState('');
+    const [acceptedPrivacyPolicy, setAcceptedPrivacyPolicy] = useState(false);
+    const [acceptedTerms, setAcceptedTerms] = useState(false);
+
     return (
         <>
             <Head>
@@ -9,7 +26,140 @@ const SignUpPage: NextPage = () => {
                 <meta name="description" content="PeopleEat - Sign Up" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main>Sign Up</main>
+            <main style={{ display: 'flex', height: '100%' }}>
+                <VStack style={{ flex: 1, padding: '32px', gap: '32px', overflowY: 'scroll' }}>
+                    <HStack style={{ width: '100%' }}>
+                        <Image src={'/people-eat-logo-title.jpeg'} alt={''} width={1429} height={376} />
+                        <Spacer />
+                    </HStack>
+
+                    <VStack style={{ width: '100%', alignItems: 'flex-start' }}>
+                        <h2>Find a private chef!</h2>
+                        <p>Please enter your details</p>
+                    </VStack>
+
+                    <HStack style={{ gap: '32px' }}>
+                        <PeopleEatIcon edgeLength={54} icon={Icon.apple} />
+                        <PeopleEatIcon edgeLength={54} icon={Icon.google} />
+                    </HStack>
+
+                    <HStack style={{ width: '100%', alignItems: 'center', gap: '32px' }}>
+                        <div style={{ height: '1px', backgroundColor: '#F5F5F5', flex: 1 }}></div>
+                        <p>with</p>
+                        <div style={{ height: '1px', backgroundColor: '#F5F5F5', flex: 1 }}></div>
+                    </HStack>
+
+                    <VStack style={{ width: '100%', alignItems: 'flex-start' }}>
+                        <p>First Name</p>
+                        <TextField
+                            style={{ width: '100%' }}
+                            value={firstName}
+                            onChange={(event): void => {
+                                setFirstName(event.target.value);
+                            }}
+                            variant="outlined"
+                        />
+                    </VStack>
+
+                    <VStack style={{ width: '100%', alignItems: 'flex-start' }}>
+                        <p>Last Name</p>
+                        <TextField
+                            style={{ width: '100%' }}
+                            value={lastName}
+                            onChange={(event): void => {
+                                setLastName(event.target.value);
+                            }}
+                            variant="outlined"
+                        />
+                    </VStack>
+
+                    <VStack style={{ width: '100%', alignItems: 'flex-start' }}>
+                        <p>Email</p>
+                        <TextField
+                            style={{ width: '100%' }}
+                            value={emailAddress}
+                            onChange={(event): void => {
+                                setEmailAddress(event.target.value);
+                            }}
+                            variant="outlined"
+                        />
+                    </VStack>
+
+                    <VStack style={{ width: '100%', alignItems: 'flex-start' }}>
+                        <p>Password</p>
+                        <TextField
+                            style={{ width: '100%' }}
+                            type="password"
+                            value={password}
+                            onChange={(event): void => {
+                                setPassword(event.target.value);
+                            }}
+                            variant="outlined"
+                        />
+                    </VStack>
+
+                    <VStack style={{ width: '100%', alignItems: 'flex-start' }}>
+                        <p>Repeat Password</p>
+                        <TextField
+                            style={{ width: '100%' }}
+                            type="password"
+                            value={passwordRepeat}
+                            onChange={(event): void => {
+                                setPasswordRepeat(event.target.value);
+                            }}
+                            variant="outlined"
+                        />
+                    </VStack>
+
+                    <VStack style={{ width: '100%', alignItems: 'flex-start', padding: '16px', borderColor: 'gray', borderWidth: 1 }}>
+                        <FormGroup>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        value={acceptedPrivacyPolicy}
+                                        onChange={(event): void => setAcceptedPrivacyPolicy(event.target.checked)}
+                                    />
+                                }
+                                label="I have read and accept the Privacy Policy"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox value={acceptedTerms} onChange={(event): void => setAcceptedTerms(event.target.checked)} />
+                                }
+                                label="I have read and accept the Terms and Conditions"
+                            />
+                        </FormGroup>
+                    </VStack>
+
+                    <Button variant="contained" style={{ width: '100%' }}>
+                        Sign up
+                    </Button>
+
+                    <HStack>
+                        You already have a profile? &nbsp;
+                        <Link href={'/sign-in'}>Sign in here</Link>
+                    </HStack>
+                </VStack>
+
+                <VStack style={{ flex: 1, backgroundImage: 'url(/picture-1.png)', backgroundPosition: 'center', backgroundSize: 'cover' }}>
+                    <Spacer />
+                    <VStack
+                        style={{
+                            width: 'calc(100% - 64px)',
+                            margin: '32px',
+                            backgroundColor: 'rgba(163, 163, 163, 0.5)',
+                            backdropFilter: 'blur(15px)',
+                            borderRadius: '16px',
+                            padding: '16px',
+                            height: '256px',
+                        }}
+                    >
+                        <HStack></HStack>
+                    </VStack>
+                </VStack>
+            </main>
         </>
     );
 };
