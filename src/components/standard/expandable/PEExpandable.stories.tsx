1c855e7ff7f3f150051b0dd12d5d4eb7f01e805c
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-24 23:33:20 +0200

    feat: add PEExpandable

---
+++ src/components/standard/expandable/PEExpandable.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PEExpandable from './PEExpandable';
+
+const meta: Meta<typeof PEExpandable> = {
+    title: 'Standard Components/PEExpandable',
+    component: PEExpandable,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PEExpandable> = {
+    args: {
+        title: 'Title',
+        description: 'Lorem ipsum dore mit',
+    },
+};
