e403cfe49f798f0ea55608156147c070103a1435
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-24 23:21:58 +0200

    feat: add PEDropdown

---
+++ src/components/standard/dropdown/PEDropdown.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PEDropdown from './PEDropdown';
+
+const meta: Meta<typeof PEDropdown> = {
+    title: 'Standard Components/PEDropdown',
+    component: PEDropdown,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PEDropdown> = {
+    args: {},
+};
