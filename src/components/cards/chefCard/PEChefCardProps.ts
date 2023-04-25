ff4c6fc7fbcfd6248d85c291fb44cc3d1a451b27
Llavrov <levlevlavrov@gmail.com>
2023-04-25 23:54:37 +0300

    feat: add PEChefCard

---
+++ src/components/cards/chefCard/PEChefCardProps.ts
@@ -0,0 +1,15 @@
+import { type CookRank } from '../../../data-source/generated/graphql';
+
+export interface PEChefCardProps {
+    firstName: string;
+    profilePictureUrl?: string;
+    rank: CookRank;
+    location: string;
+    rating: {
+        average: number;
+        count: number;
+    };
+    categories: string[];
+    kitchens: string[];
+    picturePosition?: string;
+}
