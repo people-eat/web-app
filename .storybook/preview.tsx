efd3e014f6a72c5a031329d193247ed4f2e150d3
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-26 07:48:20 +0200

    chore: setup i18n

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,20 +1,51 @@
 import { ThemeProvider } from '@mui/material';
 import type { Preview } from '@storybook/react';
+import moment from 'moment';
+import I18nProvider from 'next-translate/I18nProvider';
 import React, { type ReactElement } from 'react';
+import commonGerman from '../locales/de/common.json';
+import commonEnglish from '../locales/en/common.json';
+import commonFrench from '../locales/fr/common.json';
+import commonRussian from '../locales/ru/common.json';
 import { theme } from '../src/pages/_app';
 import '../src/styles/globals.css';
 
 export const decorators = [
-    (Story: any): ReactElement => {
+    (Story: any, context: any): ReactElement => {
+        const language = context.globals.language;
+        moment.locale(language);
+
+        let nameSpaces = { common: commonEnglish };
+
+        if (language === 'de') nameSpaces = { common: commonGerman };
+        if (language === 'ru') nameSpaces = { common: commonRussian };
+        if (language === 'fr') nameSpaces = { common: commonFrench };
+
         return (
             <ThemeProvider theme={theme}>
-                <Story />
+                <I18nProvider lang={language} namespaces={nameSpaces}>
+                    <Story />
+                </I18nProvider>
             </ThemeProvider>
         );
     },
 ];
 
 const preview: Preview = {
+    globalTypes: {
+        language: {
+            defaultValue: 'en',
+            toolbar: {
+                icon: 'globe',
+                items: [
+                    { value: 'de', right: '🇩🇪', title: 'German' },
+                    { value: 'fr', right: '🇫🇷', title: 'French' },
+                    { value: 'en', right: '🇺🇸', title: 'English' },
+                    { value: 'ru', right: '🇷🇺', title: 'Russian' },
+                ],
+            },
+        },
+    },
     parameters: {
         actions: { argTypesRegex: '^on[A-Z].*' },
         controls: {
