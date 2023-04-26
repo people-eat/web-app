13a699508669c1274775af76587a11c346cd9c23
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-26 07:48:20 +0200

    chore: setup i18n

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const config = {
      * @see https://github.com/vercel/next.js/issues/41980
      */
     i18n: {
-        locales: ['en'],
+        locales: ['de', 'fr', 'en', 'ru'],
         defaultLocale: 'en',
     },
 
