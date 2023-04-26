15a0146c77ebab433be8b73c67ecc8cb8426fdb6
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-26 18:30:53 +0200

    feat: add home page

---
+++ src/components/pages/home/section6/HomePageSection6.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import HomePageSection6 from './HomePageSection6';
+
+const meta: Meta<typeof HomePageSection6> = {
+    title: 'Home Page/Section 6',
+    component: HomePageSection6,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof HomePageSection6> = {};
---
+++ src/components/pages/home/section6/PEPointsCard.tsx
@@ -0,0 +1,42 @@
+import { type ReactElement } from 'react';
+import PEButton from '../../../standard/buttons/PEButton';
+
+export default function PEPointsCard(): ReactElement {
+    return (
+        <div className="flex flex-col big:w-[45%] justify-start items-start lg:w-full w-[580px] max-w-[580px] sm_max:bg-yellowLight sm:bg-white gap-3 p-6 box-border rounded-3 shadow-primary">
+            <h2 className={'text-heading-ss my-0 font-manrope leading-15'}>Sie können das richtige Menü nicht finden?</h2>
+            <div className={'flex gap-4 sm:flex-col w-full'}>
+                <div className={'flex gap-2 lg:border-disabled lg:items-start lg:border-solid rounded-2 lg:p-3 lg:w-full box-border'}>
+                    <span className="flex border-[1px] justify-center items-center h-[38px] min-w-[38px] border-solid border-orange text-text-s text-orange p-[9px] rounded-2 box-border">
+                        01
+                    </span>
+                    <span className={'max-w-[240px] text-preBlack text-text-s leading-5'}>
+                        Setze dein Budget sowie individuelle Präferenzen fest
+                    </span>
+                </div>
+                <div className={'flex gap-2 lg:border-disabled lg:border-solid rounded-2 lg:p-3 lg:w-full box-border'}>
+                    <span className="flex border-[1px] justify-center items-center h-[38px] min-w-[38px] border-solid border-orange text-text-s text-orange p-[9px] rounded-2 box-border">
+                        02
+                    </span>
+                    <span className={'max-w-[240px] text-preBlack text-text-s leading-5'}>
+                        Erhalte einen personalisierten Menüvorschlag
+                    </span>
+                </div>
+            </div>
+            <div className={'flex gap-2 lg:border-disabled lg:border-solid rounded-2 lg:p-3 lg:w-full box-border'}>
+                <span className="flex border-[1px] justify-center items-center h-[38px] min-w-[38px] border-solid border-orange text-text-s text-orange p-[9px] rounded-2 box-border">
+                    03
+                </span>
+                <span className={'max-w-[240px] text-preBlack text-text-s leading-5'}>
+                    Bestätige und stimme dich direkt via Chat mit deinem PeopleEat Chef ab.
+                </span>
+            </div>
+            <PEButton
+                className={'mt-8 sm:max-w-full max-w-[270px] bg-transparent'}
+                type={'secondary'}
+                onClick={(): void => undefined}
+                title={'Individuelle Anfrage senden'}
+            />
+        </div>
+    );
+}
