aefd0eb77ea9f2a4133de577521a1264753e2c09
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-26 19:42:50 +0200

    feat: add PEBulletPoint

---
+++ src/components/standard/bulletPoint/PEBulletPoint.tsx
@@ -0,0 +1,27 @@
+import { type ReactElement } from 'react';
+import HStack from '../../utility/hStack/HStack';
+import VStack from '../../utility/vStack/VStack';
+import { type Icon } from '../icon/Icon';
+import PEIcon from '../icon/PEIcon';
+
+export interface PEBulletPointProps {
+    icon: Icon;
+    title?: string;
+    text: string;
+}
+
+export default function PEBulletPoint({ icon, title, text }: PEBulletPointProps): ReactElement {
+    return (
+        <>
+            <HStack style={{ alignItems: 'center', justifyContent: 'left', gap: 16, width: '300px' }}>
+                <div className="bg-white shadow-primary" style={{ padding: 24, borderRadius: '50%' }}>
+                    <PEIcon icon={icon} edgeLength={32} />
+                </div>
+                <VStack style={{ alignItems: 'flex-start' }}>
+                    {title && <span>{title}</span>}
+                    <span style={{ color: 'rgba(31, 31, 31, 0.8)' }}>{text}</span>
+                </VStack>
+            </HStack>
+        </>
+    );
+}
