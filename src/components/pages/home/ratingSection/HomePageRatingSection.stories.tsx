ba3c8b0bf4ca19b77ba576e54f3531477717c711
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-26 18:30:53 +0200

    feat: add home page

---
+++ src/components/pages/home/ratingSection/HomePageRatingSection.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import HomePageRatingSection from './HomePageRatingSection';
+
+const meta: Meta<typeof HomePageRatingSection> = {
+    title: 'Home Page/Rating Section',
+    component: HomePageRatingSection,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof HomePageRatingSection> = {};
