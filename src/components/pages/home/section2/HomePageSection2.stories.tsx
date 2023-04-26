455235c6f1f6917eba96fadc76de2df2cadd0139
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-26 18:30:53 +0200

    feat: add home page

---
+++ src/components/pages/home/section2/HomePageSection2.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import HomePageSection2 from './HomePageSection2';
+
+const meta: Meta<typeof HomePageSection2> = {
+    title: 'Home Page/Section 2',
+    component: HomePageSection2,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof HomePageSection2> = {};
