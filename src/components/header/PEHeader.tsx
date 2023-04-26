568333306dd63843796989e0362cf0981a700400
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-26 18:27:10 +0200

    feat: add PEHeader

---
+++ src/components/header/PEHeader.tsx
@@ -0,0 +1,29 @@
+import { Button } from '@mui/material';
+import useTranslation from 'next-translate/useTranslation';
+import Image from 'next/image';
+import Link from 'next/link';
+import { type ReactElement } from 'react';
+import HStack from '../utility/hStack/HStack';
+import Spacer from '../utility/spacer/Spacer';
+
+export default function PEHeader(): ReactElement {
+    const { t } = useTranslation('common');
+
+    return (
+        <HStack gap={16} className="w-full max-w-screen-xl" style={{ alignItems: 'center', marginTop: 8 }}>
+            <Link href={'/'} className="ml-4">
+                <Image src={'/logo.svg'} alt="" width={203} height={46} />
+            </Link>
+
+            <Link href={'/how-to-chef'}>
+                <Button style={{ color: 'rgba(31, 31, 31, 0.8)' }}>{t('how-to-become-a-chef')}</Button>
+            </Link>
+
+            <Spacer />
+
+            <Link href={'/sign-in'} className="mr-4">
+                <Button style={{ color: 'rgba(31, 31, 31, 0.8)' }}>{t('sign-in')}</Button>
+            </Link>
+        </HStack>
+    );
+}
