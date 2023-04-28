490619f1147fde086ca8cab6e88c6281ff52f864
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-28 07:48:31 +0200

    feat: add how to chef page

---
+++ src/components/pages/howToChef/index.tsx
@@ -0,0 +1,38 @@
+import { Button } from '@mui/material';
+import Link from 'next/link';
+import { type ReactElement } from 'react';
+import PEFooter from '../../footer/PEFooter';
+import PEHeader from '../../header/PEHeader';
+import Spacer from '../../utility/spacer/Spacer';
+import VStack from '../../utility/vStack/VStack';
+
+export default function HowToChefPage(): ReactElement {
+    return (
+        <VStack className="w-full">
+            <PEHeader />
+            <VStack className="w-full max-w-screen-xl">
+                <VStack
+                    className="w-full"
+                    style={{
+                        backgroundImage: 'url(/picture-1.png)',
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        height: 502,
+                        borderRadius: '16px',
+                    }}
+                >
+                    <Spacer />
+                    <VStack style={{ gap: 32 }}>
+                        <span className="text-white text-2xl font-semibold">Become part of the fastest growing</span>
+                        <span className="text-white text-2xl font-semibold">network in Germany</span>
+                        <Link href={'chef-sign-up'}>
+                            <Button variant="contained">Start now</Button>
+                        </Link>
+                    </VStack>
+                    <Spacer />
+                </VStack>
+            </VStack>
+            <PEFooter />
+        </VStack>
+    );
+}
