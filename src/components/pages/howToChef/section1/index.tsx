351f38bc3f82449d45af47e3a5276624016118eb
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-04-28 07:48:31 +0200

    feat: add how to chef page

---
+++ src/components/pages/howToChef/section1/index.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import { type ReactElement } from 'react';
+import useResponsive from '../../../../hooks/useResponsive';
+import PEButton from '../../../standard/buttons/PEButton';
+import VStack from '../../../utility/vStack/VStack';
+import { header, signUpBtn } from '../translations.mock';
+
+export default function HowToChefSection1(): ReactElement {
+    const { isMobile } = useResponsive();
+    return (
+        <VStack className="w-full max-w-screen-xl lg_min:px-8 box-border">
+            <VStack
+                className="w-full md:w-screen mt-10 md:mt-0 rounded-4 md:rounded-[0px]"
+                style={{
+                    backgroundImage: 'url(/picture-1.png)',
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    height: isMobile ? 'calc(100vh - 80px)' : 502,
+                }}
+            >
+                <VStack style={{ gap: 32 }}>
+                    <div className="flex mt-[120px] md:mt-[65%] md:px-8 box-border text-heading-xxl max-w-[1040px] leading-[80px] w-full lg:justify-center">
+                        <h2 className="w-full text-white text-center text-heading-xl md:text-heading-s m-0 p-0">{header}</h2>
+                    </div>
+                    <Link href={'chef-sign-up'} className="no-underline">
+                        <PEButton className="min-w-[300px]" onClick={(): void => undefined} title={signUpBtn} />
+                    </Link>
+                </VStack>
+            </VStack>
+        </VStack>
+    );
+}
