fd48f9e5a7135178fdfca06fd4c78ab7ddff1018
Llavrov <levlevlavrov@gmail.com>
2023-04-30 20:55:04 +0300

    Revert "PEToggle"
    This reverts commit 6668448f39bad6bbe9294d59a531466d9b7fd07d.

--- src/components/standard/button/PEToggle.tsx
+++ src/components/standard/button/PEToggle.tsx
@@ -10,9 +10,14 @@ export interface PEToggleProps {
 export default function PEToggle({ onClick, active, title }: PEToggleProps): ReactElement {
     const baseClassNames =
         'w-[70px] h-[70px] flex items-center border-solid gap-2 p-3 justify-center w-full rounded-15 border-0 transition-all';
+
+    function handleClick(): void {
+        onClick();
+    }
+
     return (
         <div
-            onClick={onClick}
+            onClick={handleClick}
             className={classNames(baseClassNames, {
                 ['bg-orange']: active,
                 ['bg-base']: !active,
