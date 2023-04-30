370b3b13686cd2c912845949bfe1a83975d5b506
Llavrov <levlevlavrov@gmail.com>
2023-04-30 19:09:08 +0300

    PEBookingRequestCard: Closed / InProcess / Offer PELineButton / PEButton / PEDownloadButton

---
+++ src/components/cards/bookingRequestCard/PEBookingRequestCardClosed.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PEBookingRequestCardClosed from './PEBookingRequestCardClosed';
+
+const meta: Meta<typeof PEBookingRequestCardClosed> = {
+    title: 'Cards/PEBookingRequestCardClosed',
+    component: PEBookingRequestCardClosed,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PEBookingRequestCardClosed> = {
+    args: {
+        menuName: 'Herdsmen',
+        date: '9. April 2022, 12:20',
+        onOrderDetailsClick: (): void => undefined,
+        onShowReviewClick: (): void => undefined,
+        clientName: 'Bernd',
+        event: 'class: Bundestag',
+        price: '340$',
+        eventDate: '1. Jan 2023',
+        persons: '8',
+        time: '18:00',
+        address: 'Weierstrass 42',
+    },
+};
---
+++ src/components/cards/bookingRequestCard/PEBookingRequestCardInProcess.tsx
@@ -0,0 +1,89 @@
+import Image from 'next/image';
+import { type ReactElement } from 'react';
+import PEButton from '../../standard/button/PEButton';
+import PELineButton from '../../standard/button/PELineButton';
+import { Icon } from '../../standard/icon/Icon';
+import PEIcon from '../../standard/icon/PEIcon';
+
+export interface PEBookingRequestCardInProcessProps {
+    onOrderDetailsClick?: () => void;
+    onShowReviewClick?: () => void;
+    date?: string;
+    menuName?: string;
+    clientName?: string;
+    clientImage?: string;
+    event?: string;
+    price?: string;
+    eventDate?: string;
+    persons?: string;
+    time?: string;
+    address?: string;
+}
+
+export default function PEBookingRequestCardInProcess({
+    onOrderDetailsClick,
+    date,
+    menuName,
+    clientName,
+    clientImage,
+    event,
+    price,
+    eventDate,
+    persons,
+    time,
+    address,
+}: PEBookingRequestCardInProcessProps): ReactElement {
+    return (
+        <div
+            onClick={(): void => undefined}
+            className="flex md:w-[580px] gap-4 flex-col p-8 box-border rounded-3 shadow-primary cursor-pointer hover:shadow-active"
+        >
+            <div className="flex w-full justify-between">
+                <PELineButton title={'Order Details'} onClick={(): void => onOrderDetailsClick?.()} />
+                <span className="text-60black text-text-sm hidden md:block">{date}</span>
+            </div>
+            <span className="pt-4 text-heading-ss-bold">{menuName}</span>
+            <div className="flex gap-4">
+                <div className={'overflow-hidden rounded-3'}>
+                    {clientImage ? (
+                        <Image src={clientImage ?? ''} alt={'client image'} width={45} height={45} />
+                    ) : (
+                        <div className="flex justify-center items-center w-11 h-11 bg-base">
+                            <PEIcon icon={Icon.profileLight} edgeLength={32} />
+                        </div>
+                    )}
+                </div>
+                <div className={'flex gap-1 flex-col'}>
+                    <span className={'text-heading-ss-bold'}>{clientName}</span>
+                    <span className={'text-text-sm'}>{event}</span>
+                </div>
+            </div>
+            <span className="text-text-sm flex items-center gap-2">
+                Price: <span className="text-green text-heading-ss-bold">{price}</span>
+            </span>
+            <div className="flex">
+                <div className="flex flex-col gap-3">
+                    <span className="text-gray text-text-s md:text-text-m">
+                        Event date: <span className="text-black">{eventDate}</span>
+                    </span>
+                    <span className="text-gray text-text-s md:text-text-m">
+                        Persons: <span className="text-black">{persons}</span>
+                    </span>
+                </div>
+                <span className="w-[1px] mx-3 md:mx-4 h-[52px] bg-separator rounded-1" />
+                <div className="flex flex-col gap-3">
+                    <span className="text-gray text-text-s md:text-text-m">
+                        Time: <span className="text-black">{time}</span>
+                    </span>
+                    <span className="text-gray text-text-s md:text-text-m">
+                        Address: <span className="text-black">{address}</span>
+                    </span>
+                </div>
+            </div>
+            <div className="flex flex-row gap-3 mt-3">
+                <PEButton onClick={(): void => undefined} title={'Rehung an Kundera  sender'} size={'s'} type={'secondary'} />
+            </div>
+            <span className="text-60black text-text-s block md:hidden w-full text-end">{date}</span>
+        </div>
+    );
+}
