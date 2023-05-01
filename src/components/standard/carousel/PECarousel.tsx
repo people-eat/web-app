6766bccb369a9498ec611b90dc04a37ba99eb6d9
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-01 23:15:11 +0200

    feat: add PECarousel

---
+++ src/components/standard/carousel/PECarousel.tsx
@@ -0,0 +1,35 @@
+import { type ReactElement } from 'react';
+import Carousel from 'react-material-ui-carousel';
+
+export interface PECarouselProps {
+    images: ReactElement[];
+}
+
+export default function PECarousel({ images }: PECarouselProps): ReactElement {
+    return (
+        <Carousel
+            // sx={{
+            //     next: {
+            //         right: 0,
+            //         backgroundColor: 'white',
+            //     },
+            //     // Applies to the "prev" button wrapper
+            //     '& .MuiButtonBase-root': {
+            //         '&:hover': {
+            //             backgroundColor: 'rgba(255, 255, 255, 0.2)',
+            //         },
+            //     },
+            // }}
+            // NextIcon={<div className="w-6 h-6 flex justify-center items-center"><PEIcon icon={Icon.arrowNext} edgeLength={8} /></div>}
+            // PrevIcon={<PEIcon icon={Icon.arrowPrev} edgeLength={8} />}
+            indicators={false}
+            autoPlay={false}
+            navButtonsAlwaysInvisible={false}
+            className={'w-full h-full object-cover'}
+        >
+            {images.map((Item, i) => (
+                <div key={i}>{Item}</div>
+            ))}
+        </Carousel>
+    );
+}
