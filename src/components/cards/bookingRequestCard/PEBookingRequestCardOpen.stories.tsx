7e1678cb536d60140e64526f4e70e6dd1dd54352
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-01 23:21:23 +0200

    feat: add PEBookingRequestCard

---
+++ src/components/cards/bookingRequestCard/PEBookingRequestCardOpen.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PEBookingRequestCardOpen from './PEBookingRequestCardOpen';
+
+const meta: Meta<typeof PEBookingRequestCardOpen> = {
+    title: 'Cards/PEBookingRequestCardOpen',
+    component: PEBookingRequestCardOpen,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PEBookingRequestCardOpen> = {
+    args: {
+        menuName: 'Herdsmen',
+        date: '9. April 2022, 12:20',
+        onOrderDetailsClick: (): void => undefined,
+        onDeclineClick: (): void => undefined,
+        onAcceptClick: (): void => undefined,
+        clientName: 'Brad',
+        event: 'class: Bundestag',
+        price: '340$',
+        eventDate: '1. Jan 2023',
+        persons: '8',
+        time: '18:00',
+        address: 'Weierstrass 42',
+    },
+};
