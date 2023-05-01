3fa02fcdbe147935d9fefc6cd36a9ad7497ecd26
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-01 22:43:12 +0200

    feat: add PEMenuCard

---
+++ src/components/cards/menuCard/PEMenuCardProps.ts
@@ -0,0 +1,11 @@
+export interface PEMenuCardProps {
+    title: string;
+    description?: string;
+    imageUrls: string[];
+    pricePerPerson: number;
+    chefFirstName: string;
+    chefProfilePictureUrl?: string;
+    categories: string[];
+    kitchen?: string;
+    onClick?: () => void;
+}
