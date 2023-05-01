a7b2cf6d4776d12a3eb5c2eb4fff68426e623fe9
Llavrov <levlevlavrov@gmail.com>
2023-05-01 21:27:31 +0300

    PEReviewCardPlatform text size

--- src/components/cards/reviewCard/PEReviewCardPlatform.tsx
+++ src/components/cards/reviewCard/PEReviewCardPlatform.tsx
@@ -25,7 +25,7 @@ export default function PEReviewCardPlatform({
     return (
         <div className="flex w-[380px] md:w-[630px] gap-4 flex-col p-4 box-border rounded-3 cursor-pointer hover:shadow-primary duration-500 ease-in">
             <div className="flex justify-between">
-                <div className="flex gap-4 items-center w-full">
+                <div className="flex gap-4 w-full">
                     <div
                         className={
                             'overflow-hidden rounded-3 min-w-[75px] max-w-[75px] h-[75px] md:min-w-[220px] md:max-w-[220px] md:h-[220px]'
@@ -60,17 +60,17 @@ export default function PEReviewCardPlatform({
                     </div>
                     <div className="flex gap-2 flex-col w-full">
                         <div className="flex gap-4 justify-between w-full">
-                            <span className={'text-text-m-bold'}>{chefName}</span>
+                            <span className={'text-text-m-bold md:text-heading-ss-bold'}>{chefName}</span>
                             <div className="flex items-center gap-2 flex-row">
                                 <PEIcon icon={Icon.star} edgeLength={20} />
                                 <span className="text-preBlack">{rank ?? '0.0'}</span>
                             </div>
                         </div>
-                        <span className="text-orange text-text-sm-bold">{position}</span>
+                        <span className="text-orange text-text-sm-bold md:text-text-m-bold">{position}</span>
                         {location ? (
                             <div className={'flex items-center gap-2 flex-row'}>
                                 <PeopleEatIcon icon={Icon.markerPinOrange} edgeLength={20} />
-                                <span className="text-preBlack text-text-sm">{location}</span>
+                                <span className="text-preBlack text-text-sm md:text-text-m">{location}</span>
                             </div>
                         ) : null}
                         <span className="text-text-m hidden md:block text-preBlack">{description}</span>
