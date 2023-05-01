623c34857008be0e6882473c9eb96631ea65574b
Llavrov <levlevlavrov@gmail.com>
2023-05-01 19:46:13 +0300

    fix PEReviewCardPlatform stories

--- src/components/cards/reviewCard/PEReviewCardPlatform.stories.tsx
+++ src/components/cards/reviewCard/PEReviewCardPlatform.stories.tsx
@@ -11,9 +11,10 @@ export default meta;
 export const Component: StoryObj<typeof PEReviewCardPlatform> = {
     args: {
         chefName: 'Maximilian',
-        event: 'Cool event',
+        position: 'Cool event',
         rank: '4.9',
         date: 'June, 14 2023 ',
         description: 'PEReviewCardPlatform description',
+        location: 'Berlin',
     },
 };
