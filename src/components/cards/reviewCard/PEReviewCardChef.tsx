0d9836b244ef266a262c2a08a0b51b5f99ce4cdc
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-01 23:22:48 +0200

    feat: add PEReviewCard

---
+++ src/components/cards/reviewCard/PEReviewCardChef.tsx
@@ -0,0 +1,74 @@
+import Image from 'next/image';
+import { type ReactElement } from 'react';
+import { type CookRank } from '../../../data-source/generated/graphql';
+import { Icon } from '../../standard/icon/Icon';
+import PEIcon from '../../standard/icon/PEIcon';
+
+export interface PEReviewCardChefProps {
+    chefFirstName: string;
+    chefProfilePictureUrl?: string;
+    chefRank: CookRank;
+    customerFirstName: string;
+    occasion?: string;
+    ratingValue: string;
+    comment: string;
+    createdAt: string;
+    onClick?: () => void;
+}
+
+export default function PEReviewCardChef({
+    chefFirstName,
+    chefProfilePictureUrl,
+    chefRank,
+    customerFirstName,
+    occasion,
+    ratingValue,
+    comment,
+    createdAt,
+    onClick,
+}: PEReviewCardChefProps): ReactElement {
+    return (
+        <div
+            className="flex w-[340px] md:w-[380px] gap-4 flex-col p-8 box-border rounded-3 shadow-primary cursor-pointer hover:shadow-active duration-200 ease-in"
+            onClick={onClick}
+        >
+            <div className="flex items-center justify-between">
+                <div className="flex gap-2 items-center">
+                    <div className={'overflow-hidden rounded-3'}>
+                        {chefProfilePictureUrl ? (
+                            <Image
+                                style={{ width: '100%', objectPosition: 'center', objectFit: 'cover' }}
+                                src={chefProfilePictureUrl ?? ''}
+                                alt={'client image'}
+                                width={45}
+                                height={45}
+                            />
+                        ) : (
+                            <div className="flex justify-center items-center w-11 h-11 bg-base">
+                                <PEIcon icon={Icon.profileLight} edgeLength={32} />
+                            </div>
+                        )}
+                    </div>
+                    <div className="flex gap-4">
+                        <div className={'flex gap-1 flex-col'}>
+                            <span className={'text-heading-ss-bold'}>{customerFirstName}</span>
+                            <span className={'text-text-sm'}>{occasion}</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center gap-2 flex-row">
+                    <PEIcon icon={Icon.star} edgeLength={20} />
+                    <span className="text-preBlack">{ratingValue ?? '0.0'}</span>
+                </div>
+            </div>
+            <div>
+                to {chefRank} <span className="text-orange text-text-m-bold">{chefFirstName}</span>
+            </div>
+            <span className="text-text-m text-preBlack line-clamp-3">{comment}</span>
+            <span className="text-orange text-text-m-bold flex items-center gap-2">
+                View original review <PEIcon icon={Icon.arrowNarrowRightOrange} />
+            </span>
+            <span className="text-60black text-text-sm w-full text-end">{createdAt}</span>
+        </div>
+    );
+}
