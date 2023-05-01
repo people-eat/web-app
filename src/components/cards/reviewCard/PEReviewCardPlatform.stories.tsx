ec88125ef1b87d995727f296bb8f36d3228804d9
Llavrov <levlevlavrov@gmail.com>
2023-05-01 19:30:16 +0300

    PEReviewCard: Menu, Chef, Platform

---
+++ src/components/cards/reviewCard/PEReviewCardPlatform.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PEReviewCardPlatform from './PEReviewCardPlatform';
+
+const meta: Meta<typeof PEReviewCardPlatform> = {
+    title: 'Cards/PEReviewCardPlatform',
+    component: PEReviewCardPlatform,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PEReviewCardPlatform> = {
+    args: {
+        chefName: 'Maximilian',
+        event: 'Cool event',
+        rank: '4.9',
+        date: 'June, 14 2023 ',
+        description: 'PEReviewCardPlatform description',
+    },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { type NextPage } from 'next';
 import Head from 'next/head';
 import HomePage from '../components/pages/home';
+import PEReviewCardChef from "~/components/cards/reviewCard/PEReviewCardChef";
+import PEReviewCardMenu from "~/components/cards/reviewCard/PEReviewCardMenu";
+import PEReviewCardPlatform from "~/components/cards/reviewCard/PEReviewCardPlatform";
 
 const Index: NextPage = () => {
     return (
@@ -10,7 +13,31 @@ const Index: NextPage = () => {
                 <meta name="description" content="PeopleEat - a platform to find private chefs / cooks" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <HomePage />
+            {/*<HomePage />*/}
+            <div className="flex gap-4 p-8 flex-wrap">
+                <PEReviewCardChef
+                    chefName={'Maximilian'}
+                    event={'Cool event'}
+                    rank={'4.9'}
+                    date={'June, 14 2023 '}
+                    description={'«Wir haben das erste Mal einen Koch für ein Dinner mit unseren Freunden Zuhause gebucht. Die Buchung war...'}
+                />
+                <PEReviewCardMenu
+                    chefName={'Maximilian'}
+                    event={'Cool event'}
+                    rank={'4.9'}
+                    date={'June, 14 2023 '}
+                    description={'«Wir haben das erste Mal einen Koch für ein Dinner mit unseren Freunden Zuhause gebucht. Die Buchung war...'}
+                />
+                <PEReviewCardPlatform
+                    chefName={'Maximilian'}
+                    position={'Master chef'}
+                    rank={'4.9'}
+                    location={'Berlin'}
+                    date={'June, 14 2023 '}
+                    description={'«Wir haben das erste Mal einen Koch für ein Dinner mit unseren Freunden Zuhause gebucht. Die Buchung war...'}
+                />
+            </div>
         </>
     );
 };
