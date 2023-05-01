ae3e0531f85c0137cf5db39d0dc08b0ac18aa72e
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-01 22:43:12 +0200

    feat: add PEMenuCard

---
+++ src/components/cards/menuCard/PEMenuCard.stories.tsx
@@ -0,0 +1,111 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PEMenuCard from './PEMenuCard';
+
+const meta: Meta<typeof PEMenuCard> = {
+    title: 'Cards/PEMenuCard',
+    component: PEMenuCard,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PEMenuCard> = {
+    args: {
+        title: 'Menu title',
+        description: 'Menu description',
+        imageUrls: [
+            'https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg',
+            'https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg',
+            'https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg',
+        ],
+        pricePerPerson: 12,
+        chefFirstName: 'Loren',
+        chefProfilePictureUrl: 'https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg',
+        categories: ['category-1', 'category-2', 'category-3'],
+        kitchen: 'kitchen-1',
+        onClick: undefined,
+    },
+};
+
+export const WithOneImage: StoryObj<typeof PEMenuCard> = {
+    args: {
+        title: 'Menu title',
+        description: 'Menu description',
+        imageUrls: ['https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg'],
+        pricePerPerson: 12,
+        chefFirstName: 'Loren',
+        chefProfilePictureUrl: 'https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg',
+        categories: ['category-1', 'category-2', 'category-3'],
+        kitchen: 'kitchen-1',
+        onClick: undefined,
+    },
+};
+
+export const WithOutImages: StoryObj<typeof PEMenuCard> = {
+    args: {
+        title: 'Menu title',
+        description: 'Menu description',
+        imageUrls: [],
+        pricePerPerson: 12,
+        chefFirstName: 'Loren',
+        chefProfilePictureUrl: 'https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg',
+        categories: ['category-1', 'category-2', 'category-3'],
+        kitchen: 'kitchen-1',
+        onClick: undefined,
+    },
+};
+
+export const WithChefPicture: StoryObj<typeof PEMenuCard> = {
+    args: {
+        title: 'Menu title',
+        description: 'Menu description',
+        imageUrls: ['https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg'],
+        pricePerPerson: 12,
+        chefFirstName: 'Loren',
+        chefProfilePictureUrl: 'https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg',
+        categories: ['category-1', 'category-2', 'category-3'],
+        kitchen: 'kitchen-1',
+        onClick: undefined,
+    },
+};
+
+export const WithoutChefPicture: StoryObj<typeof PEMenuCard> = {
+    args: {
+        title: 'Menu title',
+        description: 'Menu description',
+        imageUrls: ['https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg'],
+        pricePerPerson: 12,
+        chefFirstName: 'Loren',
+        chefProfilePictureUrl: undefined,
+        categories: ['category-1', 'category-2', 'category-3'],
+        kitchen: 'kitchen-1',
+        onClick: undefined,
+    },
+};
+
+export const WithKitchen: StoryObj<typeof PEMenuCard> = {
+    args: {
+        title: 'Menu title',
+        description: 'Menu description',
+        imageUrls: ['https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg'],
+        pricePerPerson: 12,
+        chefFirstName: 'Loren',
+        chefProfilePictureUrl: 'https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg',
+        categories: ['category-1', 'category-2', 'category-3'],
+        kitchen: 'kitchen-1',
+        onClick: undefined,
+    },
+};
+
+export const WithoutKitchen: StoryObj<typeof PEMenuCard> = {
+    args: {
+        title: 'Menu title',
+        description: 'Menu description',
+        imageUrls: ['https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg'],
+        pricePerPerson: 12,
+        chefFirstName: 'Loren',
+        chefProfilePictureUrl: 'https://prykoly.ru/wp-content/uploads/2019/11/Chef9.jpg',
+        categories: ['category-1', 'category-2', 'category-3'],
+        kitchen: undefined,
+        onClick: undefined,
+    },
+};
