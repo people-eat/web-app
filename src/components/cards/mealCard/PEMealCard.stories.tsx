d7bc11e89d7b4a75cd0a1b950f498a8bdf3fb3b7
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-01 22:44:43 +0200

    feat: add PEMealCard

---
+++ src/components/cards/mealCard/PEMealCard.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PEMealCard from './PEMealCard';
+
+const meta: Meta<typeof PEMealCard> = {
+    title: 'Cards/PEMealCard',
+    component: PEMealCard,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PEMealCard> = {
+    args: {
+        title: 'Meal title',
+        description: 'Meal description',
+        imageUrl: undefined,
+        onClick: undefined,
+    },
+};
---
+++ src/components/cards/mealCard/PEMealCardMobile.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PEMealCardMobile from './PEMealCardMobile';
+
+const meta: Meta<typeof PEMealCardMobile> = {
+    title: 'Cards/PEMealCardMobile',
+    component: PEMealCardMobile,
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof PEMealCardMobile> = {
+    args: {
+        title: 'Meal title',
+        description: 'Meal description',
+        imageUrl: undefined,
+        onClick: undefined,
+    },
+};
