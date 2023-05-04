0602e000e6b310f231ad7ce7b5e1137b9fbc8630
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-04 19:37:40 +0200

    chore: update

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,15 +1,33 @@
+import moment from 'moment';
 import useTranslation from 'next-translate/useTranslation';
-import { type ReactElement } from 'react';
+import { useState, type ReactElement } from 'react';
 import PEHeader from '../../header/PEHeader';
 import PEBulletPoint from '../../standard/bulletPoint/PEBulletPoint';
 import PEFooter from '../../standard/footer/PEFooter';
 import { Icon } from '../../standard/icon/Icon';
-import PESearch from '../../standard/search/PESearch';
 import HStack from '../../utility/hStack/HStack';
 import VStack from '../../utility/vStack/VStack';
+import HomePageSearch from './search/HomePageSearch';
+
+interface GoogleMapsPlacesResult {
+    formatted_address: string;
+    geometry: {
+        location: {
+            lat: number;
+            lng: number;
+        };
+    };
+}
 
 export default function HomePage(): ReactElement {
     const { t } = useTranslation('home');
+
+    const [addressSearchText, setAddressSearchText] = useState('');
+    const [adultCount, setAdultCount] = useState(4);
+    const [childrenCount, setChildrenCount] = useState(0);
+    const [date, setDate] = useState(moment());
+    const [searchResults, setSearchResults] = useState<GoogleMapsPlacesResult[]>([]);
+
     return (
         <VStack className="w-full">
             <PEHeader />
@@ -28,7 +46,45 @@ export default function HomePage(): ReactElement {
                         <h1 className="text-white">{t('headline')}</h1>
                     </VStack>
                     <p className="text-white">{t('sub-headline')}</p>
-                    <PESearch onSearchClick={(): void => undefined} />
+                    <HomePageSearch
+                        addressSearchText={addressSearchText}
+                        onAddressSearchTextChange={(changedSearchText): void => {
+                            console.log('called with: ', changedSearchText);
+
+                            if (!changedSearchText) {
+                                setSearchResults([]);
+                                return;
+                            }
+                            setAddressSearchText(changedSearchText);
+                            fetch(
+                                encodeURI(
+                                    'google-places-api/place/textsearch/json?query="' +
+                                        addressSearchText +
+                                        '"&key=' +
+                                        (process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY ?? ''),
+                                ),
+                            )
+                                .then((response) => response.json())
+                                .then((body: { results: GoogleMapsPlacesResult[] }) => {
+                                    console.log(`\n\nreceived ${body.results.length} results:`);
+                                    body.results.forEach((result) => console.log('- ' + result.formatted_address));
+                                    setSearchResults(body.results);
+                                })
+                                .catch((error) => console.error(error));
+                        }}
+                        adultCount={adultCount}
+                        onAdultsChange={setAdultCount}
+                        childrenCount={childrenCount}
+                        onChildrenChange={setChildrenCount}
+                        date={date}
+                        onDateChange={setDate}
+                        searchResults={searchResults.map(({ formatted_address, geometry: { location } }) => ({
+                            label: formatted_address,
+                            location: { latitude: location.lat, longitude: location.lng },
+                        }))}
+                        // eslint-disable-next-line no-alert
+                        onSearchResultSelect={(selectedSearchResult): void => alert(JSON.stringify(selectedSearchResult))}
+                    />
                 </VStack>
                 <HStack className="w-full" style={{ justifyContent: 'space-evenly' }}>
                     <PEBulletPoint icon={Icon.dishes} text={t('section-1-selling-point-1')} />
---
+++ src/components/pages/home/search/HomePageSearch.tsx
@@ -0,0 +1,110 @@
+import { Autocomplete, Divider, IconButton, TextField } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers';
+import { type Moment } from 'moment';
+import useTranslation from 'next-translate/useTranslation';
+import { type ReactElement } from 'react';
+import { Icon } from '../../../standard/icon/Icon';
+import PEIcon from '../../../standard/icon/PEIcon';
+import HStack from '../../../utility/hStack/HStack';
+
+export interface AddressSearchResult {
+    label: string;
+    location: {
+        latitude: number;
+        longitude: number;
+    };
+}
+
+export interface HomePageSearchProps {
+    addressSearchText: string;
+    onAddressSearchTextChange: (changedSearchText: string) => void;
+    adultCount: number;
+    onAdultsChange: (changedAdults: number) => void;
+    childrenCount: number;
+    onChildrenChange: (changedChildren: number) => void;
+    date: Moment;
+    onDateChange: (changedDate: Moment) => void;
+    searchResults: AddressSearchResult[];
+    onSearchResultSelect: (selectedSearchResult: AddressSearchResult) => void;
+}
+
+export default function HomePageSearch({
+    addressSearchText,
+    onAddressSearchTextChange,
+    adultCount,
+    onAdultsChange,
+    childrenCount,
+    onChildrenChange,
+    date,
+    onDateChange,
+    searchResults,
+    onSearchResultSelect,
+}: HomePageSearchProps): ReactElement {
+    const { t } = useTranslation('home');
+
+    return (
+        <HStack style={{ gap: 16, backgroundColor: 'white', padding: '10px', paddingLeft: '24px', borderRadius: 64 }}>
+            <Autocomplete
+                style={{ width: 600 }}
+                freeSolo
+                disableClearable
+                options={searchResults}
+                onChange={(_event, selectedSearchResult): void => {
+                    if (typeof selectedSearchResult === 'string') return;
+                    onSearchResultSelect(selectedSearchResult);
+                }}
+                filterOptions={(options): AddressSearchResult[] => options}
+                renderInput={(params): ReactElement => (
+                    <TextField
+                        {...params}
+                        value={addressSearchText}
+                        onChange={(event): void => {
+                            onAddressSearchTextChange(event.target.value);
+                        }}
+                        variant="standard"
+                        label={t('search-city-label')}
+                        InputProps={{ disableUnderline: true, ...params.InputProps }}
+                    />
+                )}
+            />
+            <Divider orientation="vertical" />
+            {searchResults.length}
+            <Divider orientation="vertical" />
+            <TextField
+                value={adultCount}
+                onChange={(event): void => {
+                    onAdultsChange(Number(event.target.value));
+                }}
+                variant="standard"
+                label={t('search-adults-label')}
+                type="number"
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: 1 }}
+                InputProps={{ disableUnderline: true }}
+            />
+            <Divider orientation="vertical" />
+            <TextField
+                value={childrenCount}
+                onChange={(event): void => {
+                    onChildrenChange(Number(event.target.value));
+                }}
+                variant="standard"
+                label={t('search-children-label')}
+                type="number"
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: 0 }}
+                InputProps={{ disableUnderline: true }}
+            />
+            <Divider orientation="vertical" />
+            <DatePicker
+                value={date}
+                onChange={(changedDate: Moment | null): void => {
+                    if (changedDate) onDateChange(changedDate);
+                }}
+                slotProps={{ textField: { variant: 'standard', InputProps: { disableUnderline: true } } }}
+                label={t('search-date-label')}
+            />
+            <IconButton size="large" className="bg-orange">
+                <PEIcon icon={Icon.searchBar} />
+            </IconButton>
+        </HStack>
+    );
+}
