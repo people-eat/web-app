1e900f30682c3bcc1a45e93383ce349f49fd23bb
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-07 19:37:31 +0200

    feat: add PETextFields

---
+++ src/components/standard/textFields/PEMultiLineTextField.tsx
@@ -0,0 +1,39 @@
+import TextField from '@mui/material/TextField';
+import { type ReactElement } from 'react';
+import { type PEMultiLineTextFieldProps } from './PEMultiLineTextFieldProps';
+
+export default function PEMultiLineTextField({
+    value,
+    onChange,
+    validationRule,
+    placeholder,
+    disabled,
+    style,
+    className,
+}: PEMultiLineTextFieldProps): ReactElement {
+    return (
+        <TextField
+            multiline
+            value={value}
+            onChange={({ target }): void => onChange?.(target.value, validationRule ? validationRule(target.value) : true)}
+            placeholder={placeholder}
+            disabled={disabled}
+            type="text"
+            InputProps={{
+                sx: {
+                    '&.Mui-focused': {
+                        '.MuiOutlinedInput-notchedOutline': {
+                            border: '1px solid black',
+                            borderColor: 'black',
+                        },
+                    },
+                    borderRadius: '12px',
+                    minHeight: 128,
+                    alignItems: 'flex-start',
+                },
+            }}
+            style={style}
+            className={'w-full' + (className ? ' ' + className : '')}
+        />
+    );
+}
