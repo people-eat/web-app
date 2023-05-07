70733e07650ee9d184e91bde781701a45590cd7b
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-07 19:37:31 +0200

    feat: add PETextFields

---
+++ src/components/standard/textFields/PEPasswordTextFieldProps.ts
@@ -0,0 +1,11 @@
+import { CSSProperties } from 'react';
+
+export interface PEPasswordTextFieldProps {
+    password: string;
+    onChange: (changedPassword: string, changedIsValid: boolean) => void;
+    placeholder?: string;
+    disabled?: boolean;
+
+    style?: CSSProperties;
+    className?: string;
+}
---
+++ src/components/standard/textFields/PEPhoneNumberProps.ts
@@ -0,0 +1,11 @@
+import { CSSProperties } from 'react';
+
+export interface PEPhoneNumberProps {
+    phoneNumber: string;
+    onChange: (changedPhoneNumber: string, changedIsValid: boolean) => void;
+    placeholder?: string;
+    disabled?: boolean;
+
+    style?: CSSProperties;
+    className?: string;
+}
