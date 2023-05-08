863be2b9444dfa06f83af3f589d54331e3d00e68
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-08 08:01:09 +0200

    feat: add imprint page

---
+++ src/pages/imprint/index.tsx
@@ -0,0 +1,36 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { type GetServerSideProps, type NextPage } from 'next';
+import Head from 'next/head';
+import ImprintPage, { type ImprintPageProps } from '../../components/pages/imprint';
+import { GetProfileQueryDocument } from '../../data-source/generated/graphql';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { data } = await new ApolloClient({
+        uri: process.env.NEXT_PUBLIC_SERVER_URL,
+        credentials: 'include',
+        headers: { cookie: context.req.headers.cookie as string },
+        cache: new InMemoryCache(),
+        ssrMode: true,
+    }).query({ query: GetProfileQueryDocument });
+
+    return {
+        props: {
+            signedInUser: data.users.me,
+        },
+    };
+};
+
+const Index: NextPage<ImprintPageProps> = ({ signedInUser }) => {
+    return (
+        <>
+            <Head>
+                <title>PeopleEat - Imprint</title>
+                <meta name="description" content="PeopleEat - a platform to find private chefs / cooks" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <ImprintPage signedInUser={signedInUser} />
+        </>
+    );
+};
+
+export default Index;
