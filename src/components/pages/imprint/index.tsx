6b0e1f55169c03428116192c9e57af4ce39c1766
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-08 08:01:09 +0200

    feat: add imprint page

---
+++ src/components/pages/imprint/index.tsx
@@ -0,0 +1,24 @@
+import useTranslation from 'next-translate/useTranslation';
+import { type ReactElement } from 'react';
+import useResponsive from '../../../hooks/useResponsive';
+import PEFooter from '../../footer/PEFooter';
+import PEHeader from '../../header/PEHeader';
+import PEHeaderMobile from '../../header/PEHeaderMobile';
+import VStack from '../../utility/vStack/VStack';
+
+export default function ImprintPage(): ReactElement {
+    const { t } = useTranslation('common');
+    const { isMobile } = useResponsive();
+
+    return (
+        <VStack className="w-full" style={{ gap: 80 }}>
+            {isMobile ? <PEHeaderMobile /> : <PEHeader />}
+
+            <VStack className="w-full max-w-screen-xl" style={{ gap: 64, alignItems: 'flex-start' }}>
+                <h1 className="text-heading-xl m-0 p-0 max-w-screen-lg">{t('imprint')}</h1>
+            </VStack>
+
+            <PEFooter />
+        </VStack>
+    );
+}
