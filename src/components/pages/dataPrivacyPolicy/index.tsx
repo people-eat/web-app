71e7529e6202c5436b613f56f1aebba1fb892fe3
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-08 08:01:24 +0200

    feat: add data privacy policy page

---
+++ src/components/pages/dataPrivacyPolicy/index.tsx
@@ -0,0 +1,21 @@
+import useTranslation from 'next-translate/useTranslation';
+import { type ReactElement } from 'react';
+import PEFooter from '../../footer/PEFooter';
+import PEHeader from '../../header/PEHeader';
+import VStack from '../../utility/vStack/VStack';
+
+export default function DataPrivacyPolicyPage(): ReactElement {
+    const { t } = useTranslation('common');
+
+    return (
+        <VStack className="w-full" style={{ gap: 80 }}>
+            <PEHeader />
+
+            <VStack className="w-full max-w-screen-xl" style={{ gap: 64, alignItems: 'flex-start' }}>
+                <h1 className="text-heading-xl m-0 p-0 max-w-screen-lg">{t('data-privacy-policy')}</h1>
+            </VStack>
+
+            <PEFooter />
+        </VStack>
+    );
+}
