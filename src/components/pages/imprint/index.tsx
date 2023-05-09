8fde9e73b80221d801f6a41b982af36bd589cab8
Llavrov <levlevlavrov@gmail.com>
2023-05-09 04:08:20 +0300

    chore: many changes

--- src/components/pages/imprint/index.tsx
+++ src/components/pages/imprint/index.tsx
@@ -7,15 +7,33 @@ import PEHeaderMobile from '../../header/PEHeaderMobile';
 import VStack from '../../utility/vStack/VStack';
 
 export default function ImprintPage(): ReactElement {
-    const { t } = useTranslation('common');
+    const { t } = useTranslation('imprint');
     const { isMobile } = useResponsive();
 
     return (
-        <VStack className="w-full" style={{ gap: 80 }}>
+        <VStack className="w-full min-h-screen gap-16">
             {isMobile ? <PEHeaderMobile /> : <PEHeader />}
 
-            <VStack className="w-full max-w-screen-xl" style={{ gap: 64, alignItems: 'flex-start' }}>
+            <VStack className="w-full max-w-screen-xl gap-4 lg:px-4 box-border" style={{ alignItems: 'flex-start' }}>
                 <h1 className="text-heading-xl m-0 p-0 max-w-screen-lg">{t('imprint')}</h1>
+                <p className="leading-16">
+                    {t('point-1')}
+                    <br />
+                    <br />
+                    {t('point-2')}
+                    <br />
+                    <br />
+                    {t('point-3')}
+                    <br />
+                    <br />
+                    {t('point-4')}
+                    <br />
+                    <br />
+                    {t('point-5')}
+                    <br />
+                    <br />
+                    {t('point-6')}
+                </p>
             </VStack>
 
             <PEFooter />
