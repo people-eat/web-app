9a589845f7705902908bc88f84e131baed7e3f45
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-05-10 07:48:09 +0200

    feat: add individual request page

---
+++ src/components/pages/individualRequest/step1/IndividualRequestPageStep1.stories.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import type { Meta, StoryObj } from '@storybook/react';
+import moment from 'moment';
+import { useState } from 'react';
+import IndividualRequestPageStep1 from './IndividualRequestPageStep1';
+
+const meta: Meta<typeof IndividualRequestPageStep1> = {
+    title: 'Individual Request Page/Step 1',
+    component: IndividualRequestPageStep1,
+    render: () => {
+        const [adultCount, setAdultCount] = useState(4);
+        const [childrenCount, setChildrenCount] = useState(0);
+        const [dateTime, setDateTime] = useState(moment());
+        const [occasion, setOccasion] = useState('');
+        const [budget, setBudget] = useState('');
+        const [message, setMessage] = useState('');
+
+        const [addressSearchText, setAddressSearchText] = useState<string>('');
+
+        return (
+            <IndividualRequestPageStep1
+                adultCount={adultCount}
+                setAdultCount={setAdultCount}
+                childrenCount={childrenCount}
+                setChildrenCount={setChildrenCount}
+                addressSearchText={addressSearchText}
+                setAddressSearchText={setAddressSearchText}
+                dateTime={dateTime}
+                setDateTime={setDateTime}
+                occasion={occasion}
+                setOccasion={setOccasion}
+                budget={budget}
+                setBudget={setBudget}
+                message={message}
+                setMessage={setMessage}
+                // eslint-disable-next-line no-alert
+                onContinue={(): void => alert('Continue')}
+            />
+        );
+    },
+};
+
+export default meta;
+
+export const Component: StoryObj<typeof IndividualRequestPageStep1> = {};
