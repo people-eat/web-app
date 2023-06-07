bad1c81f79d0cc7af007467b67f3fa65225778db
Llavrov <levlevlavrov@gmail.com>
2023-06-08 02:21:45 +0300

    remove callback PEImagePicker

--- src/components/standard/filePicker/PEImagePicker.tsx
+++ src/components/standard/filePicker/PEImagePicker.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
-import { useEffect, type ReactElement } from 'react';
+import { useEffect, useState, type ReactElement } from 'react';
 import { useFilePicker } from 'use-file-picker';
 import VStack from '../../utility/vStack/VStack';
 import { Icon } from '../icon/Icon';
 import PEIcon from '../icon/PEIcon';
+import PEIconButton from '../iconButton/PEIconButton';
 
 interface PEImagePickerProps {
     onDownloaded?: (file: string) => void;
@@ -19,21 +20,37 @@ export default function PEImagePicker({ onDownloaded }: PEImagePickerProps): Rea
         maxFileSize: 12,
     });
 
+    const [imageFile, setImageFile] = useState('');
+
     function handleSelectFiles(): void {
         openFileSelector();
     }
 
+    function handleRemoveImage(event: React.MouseEvent<HTMLDivElement>): void {
+        event.stopPropagation();
+        setImageFile('');
+    }
+
     useEffect(() => {
         if (filesContent[0]) onDownloaded?.(filesContent[0].content);
+        if (filesContent[0]) setImageFile(filesContent[0].content);
     }, [filesContent, onDownloaded]);
 
     return (
         <VStack style={{ alignItems: 'flex-start' }}>
             <div
                 onClick={handleSelectFiles}
-                className="flex overflow-hidden items-center w-[200px] h-[200px] hover:cursor-pointer select-none hover:shadow-primary active:shadow-active delay-100 ease-linear transition border-solid border-[1px] border-disabled justify-center rounded-4"
+                className="relative flex overflow-hidden items-center w-[200px] h-[200px] hover:cursor-pointer select-none hover:shadow-primary active:shadow-active delay-100 ease-linear transition border-solid border-[1px] border-disabled justify-center rounded-4"
             >
-                {filesContent[0] ? (
+                {imageFile && (
+                    <div
+                        onClick={(event): void => handleRemoveImage(event)}
+                        className="flex justify-center items-center opacity-50 hover:opacity-100 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                    >
+                        <PEIconButton icon={Icon.trash} withoutShadow />
+                    </div>
+                )}
+                {imageFile ? (
                     <Image
                         style={{ width: '100%', objectPosition: 'center', objectFit: 'cover' }}
                         src={filesContent[0].content}
