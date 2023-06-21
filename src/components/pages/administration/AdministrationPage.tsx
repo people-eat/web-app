fe4d906ae9426b549c0884620bb07d47a3a5eaef
Cem Yilmaz <cem.yilmaz@sap.com>
2023-06-22 00:26:28 +0200

    feat: add administration page

---
+++ src/components/pages/administration/AdministrationPage.tsx
@@ -0,0 +1,59 @@
+import { ButtonBase, Paper } from '@mui/material';
+import Link from 'next/link';
+import { type CSSProperties, type ReactElement } from 'react';
+import { type SignedInUser } from '../../../shared/SignedInUser';
+import PEFooter from '../../footer/PEFooter';
+import PEHeader from '../../header/PEHeader';
+import HStack from '../../utility/hStack/HStack';
+import Spacer from '../../utility/spacer/Spacer';
+import VStack from '../../utility/vStack/VStack';
+
+export interface AdministrationPageProps {
+    signedInUser?: SignedInUser;
+}
+
+export default function AdministrationPage({ signedInUser }: AdministrationPageProps): ReactElement {
+    const tiles: string[] = [
+        'Users',
+        'Chefs',
+        'Administrators',
+        'Statistics',
+        'Feature Toggles',
+        'Search Requests',
+        'Categories',
+        'Languages',
+        'Allergies',
+    ];
+
+    const tileStyle: CSSProperties = {
+        padding: 8,
+        width: 180,
+        height: 180,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+    };
+
+    return (
+        <VStack className="w-full min-h-screen" gap={64}>
+            <PEHeader signedInUser={signedInUser} />
+
+            <HStack gap={32} style={{ flexWrap: 'wrap', margin: 16 }} className="max-w-screen-xl">
+                {tiles.map((tile, index) => (
+                    <Link key={index} href="/" className="no-underline">
+                        <ButtonBase>
+                            <Paper style={tileStyle} elevation={3} className="text-heading-m">
+                                {tile}
+                            </Paper>
+                        </ButtonBase>
+                    </Link>
+                ))}
+            </HStack>
+
+            <Spacer />
+
+            <PEFooter />
+        </VStack>
+    );
+}
---
+++ src/pages/administration/index.tsx
@@ -0,0 +1,35 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { type GetServerSideProps, type NextPage } from 'next';
+import Head from 'next/head';
+import AdministrationPage, { type AdministrationPageProps } from '../../components/pages/administration/AdministrationPage';
+import { GetProfileQueryDocument } from '../../data-source/generated/graphql';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { data } = await new ApolloClient({
+        uri: process.env.NEXT_PUBLIC_SERVER_URL,
+        credentials: 'include',
+        headers: { cookie: context.req.headers.cookie as string },
+        cache: new InMemoryCache(),
+        ssrMode: true,
+    }).query({ query: GetProfileQueryDocument });
+
+    return {
+        props: {
+            signedInUser: data.users.me,
+        },
+    };
+};
+
+const Index: NextPage<AdministrationPageProps> = ({ signedInUser }) => {
+    return (
+        <>
+            <Head>
+                <title>PeopleEat - Administration</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <AdministrationPage signedInUser={signedInUser} />
+        </>
+    );
+};
+
+export default Index;
