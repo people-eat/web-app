9eef3ffe081eeae3e91f1409b5057b7aee8979ed
Llavrov <51132146+Llavrov@users.noreply.github.com>
2023-06-26 21:02:39 +0300

    popUp for chef-profile tab

--- src/components/pages/chefProfile/personalTab/certificationsSection/ChefProfileSection3.tsx
+++ src/components/pages/chefProfile/personalTab/certificationsSection/ChefProfileSection3.tsx
@@ -9,10 +9,24 @@ import PEModalPopUp from '../../../../standard/modal/PEModalPopUp';
 import PETextField from '../../../../standard/textFields/PETextField';
 import HStack from '../../../../utility/hStack/HStack';
 import VStack from '../../../../utility/vStack/VStack';
+import Dialog from "@mui/material/Dialog";
+import List from "@mui/material/List";
+import PEImagePicker from "../../../../standard/imagePicker/PEImagePicker";
+import useResponsive from "../../../../../hooks/useResponsive";
+import {TransitionProps} from "@mui/material/transitions";
+import Slide from "@mui/material/Slide";
+
+const Transition = forwardRef(function Transition(props: TransitionProps & { children: ReactElement }, ref: React.Ref<unknown>) {
+    return (
+        <Slide direction="up" ref={ref} {...props}>
+            {props.children}
+        </Slide>
+    );
+});
 
 export default function ChefProfileCertificationsSection(): ReactElement {
-    const { t } = useTranslation('chef-profile');
     const { isMobile } = useResponsive();
+    const { t } = useTranslation('chef-profile');
 
     const [openPopUp, setOpenPopUp] = useState(false);
     const [editMode, _setEditMode] = useState<null | number>(null);
@@ -41,41 +55,6 @@ export default function ChefProfileCertificationsSection(): ReactElement {
             className="w-full bg-white shadow-primary box-border p-8 md:p-4 rounded-4 gap-3"
             style={{ alignItems: 'center', justifyContent: 'flex-start' }}
         >
-            <PEModalPopUp width={isMobile ? '100%' : '750px'} openMenu={openPopUp} handleOpenMenu={handleUnSaveChefName}>
-                <VStack className="w-[750px] md:w-full px-10 md:p-4 py-15 box-border relative">
-                    <h2 className="m-0 mt-[-40px] pb-5 w-full text-heading-ss">{t('popup-training')}</h2>
-                    <VStack className="w-full gap-2 pb-5">
-                        <h2 className="m-0 w-full text-heading-ss">{t('popup-training-title')}</h2>
-                        <h2 className="m-0 w-full text-text-m text-disabled">{t('popup-training-pre-title')}</h2>
-                    </VStack>
-                    <VStack className="w-full gap-4" style={{ alignItems: 'flex-start' }}>
-                        <PETextField
-                            type={'text'}
-                            value={diplomaName}
-                            placeholder={t('popup-training-field01')}
-                            onChange={(value): void => setDiplomaName(value)}
-                        />
-                        <PETextField
-                            type={'text'}
-                            value={data}
-                            placeholder={t('popup-training-field02')}
-                            onChange={(value): void => setData(value)}
-                        />
-                        <h2 className="m-0 w-full text-heading-ss">{t('popup-training-photo')}</h2>
-                        <VStack className="w-[200px] h-[200px] hover:cursor-pointer select-none hover:shadow-primary active:shadow-active delay-100 ease-linear transition border-solid border-[1px] border-disabled justify-center rounded-4">
-                            <PEIcon icon={Icon.plus} />
-                        </VStack>
-                        <VStack className="w-full">
-                            <p>{t('popup-training-displayed-info')}</p>
-                            {editMode ? (
-                                <PEButton className="max-w-[250px]" onClick={handleSaveDiploma} title={t('tickets-button')} />
-                            ) : (
-                                <PEButton className="max-w-[250px]" onClick={handleAddNewDiploma} title={t('tickets-button')} />
-                            )}
-                        </VStack>
-                    </VStack>
-                </VStack>
-            </PEModalPopUp>
             <HStack className="w-full" style={{ alignItems: 'center', justifyContent: 'space-between' }}>
                 <p className="text-heading-ss w-full justify-start my-0">{t('popup-training')}</p>
                 <PEIconButton onClick={(): void => setOpenPopUp(!openPopUp)} icon={Icon.plus} iconSize={24} withoutShadow />
@@ -89,6 +68,114 @@ export default function ChefProfileCertificationsSection(): ReactElement {
                     <PEIconButton onClick={handleOpenModal} icon={Icon.editPencil} iconSize={24} withoutShadow />
                 </HStack>
             ))}
+
+            {!isMobile && (
+                <PEModalPopUp width={isMobile ? '100%' : '750px'} openMenu={openPopUp} handleOpenMenu={handleUnSaveChefName}>
+                    <VStack className="w-[750px] md:w-full px-10 md:p-4 py-15 box-border relative">
+                        <h2 className="m-0 mt-[-40px] pb-5 w-full text-heading-ss">{t('popup-training')}</h2>
+                        <VStack className="w-full gap-2 pb-5">
+                            <h2 className="m-0 w-full text-heading-ss">{t('popup-training-title')}</h2>
+                            <h2 className="m-0 w-full text-text-m text-disabled">{t('popup-training-pre-title')}</h2>
+                        </VStack>
+                        <VStack className="w-full gap-4" style={{ alignItems: 'flex-start' }}>
+                            <PETextField
+                                type={'text'}
+                                value={diplomaName}
+                                placeholder={t('popup-training-field01')}
+                                onChange={(value): void => setDiplomaName(value)}
+                            />
+                            <PETextField
+                                type={'text'}
+                                value={data}
+                                placeholder={t('popup-training-field02')}
+                                onChange={(value): void => setData(value)}
+                            />
+                            <h2 className="m-0 w-full text-heading-ss">{t('popup-training-photo')}</h2>
+                            <VStack className="w-[200px] h-[200px] hover:cursor-pointer select-none hover:shadow-primary active:shadow-active delay-100 ease-linear transition border-solid border-[1px] border-disabled justify-center rounded-4">
+                                <PEIcon icon={Icon.plus} />
+                            </VStack>
+                            <VStack className="w-full">
+                                <p>{t('popup-training-displayed-info')}</p>
+                                {editMode ? (
+                                    <PEButton className="max-w-[250px]" onClick={handleSaveDiploma} title={t('tickets-button')} />
+                                ) : (
+                                    <PEButton className="max-w-[250px]" onClick={handleAddNewDiploma} title={t('tickets-button')} />
+                                )}
+                            </VStack>
+                        </VStack>
+                    </VStack>
+                </PEModalPopUp>
+            )}
+
+            {isMobile && (
+                <Dialog
+                    sx={{
+                        height: '100vh',
+                        width: '100%',
+                        minHeight: '90%',
+                        minWidth: '100%',
+                        overflow: 'hidden',
+                        position: 'relative',
+                        '& .MuiPaper-root': {
+                            margin: '5vh 0 0',
+                            borderRadius: '16px 16px 0 0',
+                            padding: '16px',
+                            boxSizing: 'border-box',
+                            minHeight: '95vh',
+                            minWidth: '100%',
+                        },
+                    }}
+                    open={openPopUp}
+                    onClose={(): void => setOpenPopUp(false)}
+                    TransitionComponent={Transition}
+                    aria-labelledby="draggable-dialog-component"
+                >
+                    <List
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            flexDirection: 'column',
+                            minHeight: '90%',
+                            minWidth: '100%',
+                            height: '80vh',
+                        }}
+                    >
+                        <VStack className="w-[750px] md:w-full px-10 md:p-4 py-15 box-border relative">
+                            <h2 className="m-0 mt-[-40px] pb-5 w-full text-heading-ss">{t('popup-training')}</h2>
+                            <VStack className="w-full gap-2 pb-5">
+                                <h2 className="m-0 w-full text-heading-ss">{t('popup-training-title')}</h2>
+                                <h2 className="m-0 w-full text-text-m text-disabled">{t('popup-training-pre-title')}</h2>
+                            </VStack>
+                            <VStack className="w-full gap-4" style={{ alignItems: 'flex-start' }}>
+                                <PETextField
+                                    type={'text'}
+                                    value={diplomaName}
+                                    placeholder={t('popup-training-field01')}
+                                    onChange={(value): void => setDiplomaName(value)}
+                                />
+                                <PETextField
+                                    type={'text'}
+                                    value={data}
+                                    placeholder={t('popup-training-field02')}
+                                    onChange={(value): void => setData(value)}
+                                />
+                                <h2 className="m-0 w-full text-heading-ss">{t('popup-training-photo')}</h2>
+                                <VStack className="w-[200px] h-[200px] hover:cursor-pointer select-none hover:shadow-primary active:shadow-active delay-100 ease-linear transition border-solid border-[1px] border-disabled justify-center rounded-4">
+                                    <PEIcon icon={Icon.plus} />
+                                </VStack>
+                                <VStack className="w-full">
+                                    <p>{t('popup-training-displayed-info')}</p>
+                                    {editMode ? (
+                                        <PEButton className="max-w-[250px]" onClick={handleSaveDiploma} title={t('tickets-button')} />
+                                    ) : (
+                                        <PEButton className="max-w-[250px]" onClick={handleAddNewDiploma} title={t('tickets-button')} />
+                                    )}
+                                </VStack>
+                            </VStack>
+                        </VStack>
+                    </List>
+                </Dialog>
+            )}
         </VStack>
     );
 }
--- src/components/pages/chefProfile/personalTab/section1/ChefProfileSection1.tsx
+++ src/components/pages/chefProfile/personalTab/section1/ChefProfileSection1.tsx
@@ -1,8 +1,12 @@
 import { useMutation } from '@apollo/client';
+import Dialog from '@mui/material/Dialog';
+import List from '@mui/material/List';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
 import useTranslation from 'next-translate/useTranslation';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState, type ReactElement } from 'react';
+import React, { forwardRef, useState, type ReactElement } from 'react';
 import { UpdateUserProfilePictureDocument, type CookRank } from '../../../../../data-source/generated/graphql';
 import useResponsive from '../../../../../hooks/useResponsive';
 import PEButton from '../../../../standard/buttons/PEButton';
@@ -33,6 +37,14 @@ export interface ChefProfileSection1Props {
     refetch: () => void;
 }
 
+const Transition = forwardRef(function Transition(props: TransitionProps & { children: ReactElement }, ref: React.Ref<unknown>) {
+    return (
+        <Slide direction="up" ref={ref} {...props}>
+            {props.children}
+        </Slide>
+    );
+});
+
 export default function ChefProfileSection1({ chefProfile, refetch }: ChefProfileSection1Props): ReactElement {
     const { t } = useTranslation('chef-profile');
     const { isMobile } = useResponsive();
@@ -47,6 +59,10 @@ export default function ChefProfileSection1({ chefProfile, refetch }: ChefProfil
     const [editedProfilePicture, setEditedProfilePicture] = useState<File | undefined | null>(null);
 
     const { t: commonTranslate } = useTranslation('common');
+
+    const handleClose = (): void => {
+        setEdit(false);
+    };
 
     function handleUnSaveChefName(): void {
         setEditFirstName(chefProfile.user.firstName);
@@ -165,26 +181,83 @@ export default function ChefProfileSection1({ chefProfile, refetch }: ChefProfil
                 )}
             </VStack>
 
-            <PEModalPopUp width={isMobile ? '100%' : 750} openMenu={edit} handleOpenMenu={handleUnSaveChefName}>
-                <VStack className="w-[750px] md:w-full md:h-full px-10 md:px-4 py-15 md:py-4 box-border relative">
-                    <h2 className="m-0 pb-5 w-full">Change profile info</h2>
-                    <VStack className="w-full gap-4" style={{ alignItems: 'flex-start', justifyContent: 'center' }}>
-                        <PETextField type={'text'} value={editFirstName} onChange={(value): void => setEditFirstName(value)} />
-                        <PETextField type={'text'} value={editLastName} onChange={(value): void => setEditLastName(value)} />
-                        <PEImagePicker
-                            onPick={setEditedProfilePicture}
-                            defaultImage={image}
-                            onRemoveDefaultImage={(): void => setEditedProfilePicture(undefined)}
+            {!isMobile && (
+                <PEModalPopUp width={isMobile ? '100%' : 750} openMenu={edit} handleOpenMenu={handleUnSaveChefName}>
+                    <VStack className="w-[750px] md:w-full md:h-full px-10 md:px-4 py-15 md:py-4 box-border relative">
+                        <h2 className="m-0 pb-5 w-full">Change profile info</h2>
+                        <VStack className="w-full gap-4" style={{ alignItems: 'flex-start', justifyContent: 'center' }}>
+                            <PETextField type={'text'} value={editFirstName} onChange={(value): void => setEditFirstName(value)} />
+                            <PETextField type={'text'} value={editLastName} onChange={(value): void => setEditLastName(value)} />
+                            <PEImagePicker
+                                onPick={setEditedProfilePicture}
+                                defaultImage={image}
+                                onRemoveDefaultImage={(): void => setEditedProfilePicture(undefined)}
+                            />
+                        </VStack>
+                        <PEButton
+                            className="max-w-[250px] mt-10"
+                            onClick={handleSaveProfileInfo}
+                            title="Save"
+                            disabled={editedProfilePicture === null && firstName === editFirstName && lastName === editLastName}
                         />
                     </VStack>
-                    <PEButton
-                        className="max-w-[250px] mt-10"
-                        onClick={handleSaveProfileInfo}
-                        title="Save"
-                        disabled={editedProfilePicture === null && firstName === editFirstName && lastName === editLastName}
-                    />
-                </VStack>
-            </PEModalPopUp>
+                </PEModalPopUp>
+            )}
+
+            {isMobile && (
+                <Dialog
+                    sx={{
+                        height: '100vh',
+                        width: '100%',
+                        minHeight: '90%',
+                        minWidth: '100%',
+                        overflow: 'hidden',
+                        position: 'relative',
+                        '& .MuiPaper-root': {
+                            margin: '5vh 0 0',
+                            borderRadius: '16px 16px 0 0',
+                            padding: '16px',
+                            boxSizing: 'border-box',
+                            minHeight: '95vh',
+                            minWidth: '100%',
+                        },
+                    }}
+                    open={edit}
+                    onClose={handleClose}
+                    TransitionComponent={Transition}
+                    aria-labelledby="draggable-dialog-component"
+                >
+                    <List
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            flexDirection: 'column',
+                            minHeight: '90%',
+                            minWidth: '100%',
+                            height: '80vh',
+                        }}
+                    >
+                        <VStack className="w-[750px] md:w-full md:h-full px-10 md:px-4 py-15 md:py-4 box-border relative">
+                            <h2 className="m-0 pb-5 w-full">Change profile info</h2>
+                            <VStack className="w-full gap-4" style={{ alignItems: 'flex-start', justifyContent: 'center' }}>
+                                <PETextField type={'text'} value={editFirstName} onChange={(value): void => setEditFirstName(value)} />
+                                <PETextField type={'text'} value={editLastName} onChange={(value): void => setEditLastName(value)} />
+                                <PEImagePicker
+                                    onPick={setEditedProfilePicture}
+                                    defaultImage={image}
+                                    onRemoveDefaultImage={(): void => setEditedProfilePicture(undefined)}
+                                />
+                            </VStack>
+                            <PEButton
+                                className="max-w-[250px] mt-10"
+                                onClick={handleSaveProfileInfo}
+                                title="Save"
+                                disabled={editedProfilePicture === null && firstName === editFirstName && lastName === editLastName}
+                            />
+                        </VStack>
+                    </List>
+                </Dialog>
+            )}
         </>
     );
 }
