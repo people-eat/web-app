f1c9e7ca4e9f99ea56d9d746ca3f982bf30eeced
Llavrov <51132146+Llavrov@users.noreply.github.com>
2023-06-26 20:46:05 +0300

    profile setEditedProfilePicture

--- src/components/pages/chefProfile/personalTab/ChefProfilePagePersonalTab.tsx
+++ src/components/pages/chefProfile/personalTab/ChefProfilePagePersonalTab.tsx
@@ -114,7 +114,7 @@ export default function ChefProfilePagePersonalTab({ cookId }: { cookId: string 
         <VStack className="w-full max-w-screen-xl mb-[80px] lg_min:my-10 md:my-2 gap-6 md:gap-3 lg:px-5 box-border">
             {chefProfile && (
                 <>
-                    <ChefProfileSection1 chefProfile={chefProfile} refetchChefData={refetch} />
+                    <ChefProfileSection1 chefProfile={chefProfile} refetch={(): void => void refetch()} />
 
                     <HStack
                         className="w-full bg-white shadow-primary box-border p-8 md:p-2 rounded-4"
