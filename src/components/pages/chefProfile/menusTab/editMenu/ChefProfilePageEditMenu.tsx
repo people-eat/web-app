72ae8252c9fa6fa9c945d34e041e8eb8194f747f
Llavrov <51132146+Llavrov@users.noreply.github.com>
2023-06-27 19:22:46 +0300

    title for edit menu page

--- src/components/pages/chefProfile/menusTab/editMenu/ChefProfilePageEditMenu.tsx
+++ src/components/pages/chefProfile/menusTab/editMenu/ChefProfilePageEditMenu.tsx
@@ -36,7 +36,7 @@ export default function ChefProfilePageEditMenu({ cookId, menuId, onSaveUpdates 
                 </div>
 
                 <VStack className="w-full mb-6" style={{ alignItems: 'flex-start' }}>
-                    <p className="w-full text-heading-xl md:text-heading-s my-0 mb-6">Adding a new menu</p>
+                    <p className="w-full text-heading-xl md:text-heading-s my-0 mb-6">Edit menu info</p>
                     <Stepper activeStep={step}>
                         <Step>
                             <StepLabel onClick={(): void => setStep(0)}>Step 1</StepLabel>
