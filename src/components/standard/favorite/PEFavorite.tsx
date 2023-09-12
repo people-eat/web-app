d77b4a88348cc2dafadb17bb308a47b63027ea11
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-09-12 08:17:07 +0200

    Update src/components/standard/favorite/PEFavorite.tsx

--- src/components/standard/favorite/PEFavorite.tsx
+++ src/components/standard/favorite/PEFavorite.tsx
@@ -7,7 +7,7 @@ import { type PEFavoriteProps } from './PEFavoriteProps';
 
 export default function PEFavorite({ disabled, isFavorite, onIsFavoriteChange }: PEFavoriteProps): ReactElement {
     return (
-        <div className={classNames('w-10_ h-10_ rounded-2', 'bg-base rounded-2', 'favorite')}>
+        <div className={classNames('w-10_ h-10_ rounded-2', 'bg-base rounded-2')}>
             <IconButton disabled={disabled} onClick={(): void => onIsFavoriteChange(!isFavorite)}>
                 <PEIcon icon={isFavorite ? Icon.heartFull : Icon.heart} />
             </IconButton>
