a0f510c25908381b23a861fedb3d2b86529779a1
Alex <alexgrubor@gmail.com>
2023-09-14 11:52:25 +0200

    globalbooking cards plus translation fix on chef profile and on menu booking request date, time, location,occasion and message label translation keys

--- src/components/pages/profile/globalBookingRequestsTab/ProfilePageGlobalBookingRequestsTab.tsx
+++ src/components/pages/profile/globalBookingRequestsTab/ProfilePageGlobalBookingRequestsTab.tsx
@@ -9,7 +9,8 @@ import {
     type CurrencyCode,
 } from '../../../../data-source/generated/graphql';
 import BookingRequestDetailsDialog from '../../../BookingRequestDetailsDialog';
-import PEBookingRequestCardOpen from '../../../cards/bookingRequestCard/PEBookingRequestCardOpen';
+// import PEBookingRequestCardOpen from '../../../cards/bookingRequestCard/PEBookingRequestCardOpen';
+import GlobalBookingRequestCardCustomer from '../../../cards/globalBookingRequests/GlobalBookingRequestCardCustomer';
 import HStack from '../../../utility/hStack/HStack';
 import VStack from '../../../utility/vStack/VStack';
 
@@ -19,7 +20,6 @@ export interface ProfilePageGlobalBookingRequestsTabProps {
 
 export default function ProfilePageGlobalBookingRequestsTab({ userId }: ProfilePageGlobalBookingRequestsTabProps): ReactElement {
     const { t } = useTranslation('common');
-    const { t: bookingTranslations } = useTranslation('global-booking-request');
     const [updateBookingRequestPrice] = useMutation(UserBookingRequestUpdatePriceDocument);
 
     const [selectedBookingRequest, setSelectedBookingRequest] = useState<
@@ -46,7 +46,7 @@ export default function ProfilePageGlobalBookingRequestsTab({ userId }: ProfileP
 
     return (
         <VStack className="w-full md:overflow-hidden relative max-w-screen-xl gap-6 lg:px-4 md:py-6 box-border">
-            <HStack className="w-full gap-8 flex-wrap" style={{ justifyContent: 'space-between' }}>
+            {/* <HStack className="w-full gap-8 flex-wrap" style={{ justifyContent: 'space-between' }}>
                 {globalBookingRequests?.map((globalBookingRequest) => (
                     <div key={globalBookingRequest.globalBookingRequestId} className="w-[calc(50%-20px)] md:w-full">
                         <PEBookingRequestCardOpen
@@ -60,6 +60,22 @@ export default function ProfilePageGlobalBookingRequestsTab({ userId }: ProfileP
                             dateTime={moment(globalBookingRequest.dateTime)}
                             participants={globalBookingRequest.adultParticipants + globalBookingRequest.children}
                             address={'Location'}
+                        />
+                    </div>
+                ))}
+            </HStack> */}
+            <HStack className="w-full gap-8 flex-wrap" style={{ justifyContent: 'space-between' }}>
+                {globalBookingRequests?.map((globalBookingRequest) => (
+                    <div key={globalBookingRequest.globalBookingRequestId} className="w-[calc(50%-20px)] md:w-full">
+                        <GlobalBookingRequestCardCustomer
+                            createdAt={moment(globalBookingRequest.createdAt)}
+                            occasion={globalBookingRequest.occasion}
+                            price={`${globalBookingRequest.price.amount} ${globalBookingRequest.price.currencyCode}`}
+                            dateTime={moment(globalBookingRequest.dateTime)}
+                            adults={globalBookingRequest.adultParticipants}
+                            numOfChildren={globalBookingRequest.children}
+                            participants={globalBookingRequest.adultParticipants + globalBookingRequest.children}
+                            address={'address'}
                         />
                     </div>
                 ))}
