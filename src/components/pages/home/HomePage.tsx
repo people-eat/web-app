9537b194d55e38f71a01423af9e5ea78b87d3e85
Cem Yilmaz <cem.yilmaz@sap.com>
2023-12-04 08:12:17 +0100

    make home page pictures unoptimized

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -164,7 +164,14 @@ export function HomePage({ signedInUser, searchParameters, heroCooks, heroMenus 
                     <h2 className={styles.newSectionTitle}>Finde in nur wenigen Schritten einen Koch für Zuhause</h2>
                     <div className={styles.newSectionBody}>
                         <div className={styles.newSectionEntry}>
-                            <Image src="/home/2/köche.png" alt="" width={380} height={240} className={styles.newSectionEntryImage} />
+                            <Image
+                                unoptimized
+                                src="/home/2/köche.png"
+                                alt=""
+                                width={380}
+                                height={240}
+                                className={styles.newSectionEntryImage}
+                            />
                             <h3 className={styles.newSectionEntryTitle}>Finde einen Koch in deiner Umgebung</h3>
                             <p className={styles.newSectionEntryDescription}>
                                 Wähle einen Koch oder aus verschiedenen Menüvorschlägen aus. Wenn du nach einer speziellen Küche oder einer
@@ -174,6 +181,7 @@ export function HomePage({ signedInUser, searchParameters, heroCooks, heroMenus 
                         </div>
                         <div className={styles.newSectionEntry}>
                             <Image
+                                unoptimized
                                 src="/home/2/menü-gerichte.png"
                                 alt=""
                                 width={380}
@@ -188,7 +196,14 @@ export function HomePage({ signedInUser, searchParameters, heroCooks, heroMenus 
                             </p>
                         </div>
                         <div className={styles.newSectionEntry}>
-                            <Image src="/home/2/chat.png" alt="" width={380} height={240} className={styles.newSectionEntryImage} />
+                            <Image
+                                unoptimized
+                                src="/home/2/chat.png"
+                                alt=""
+                                width={380}
+                                height={240}
+                                className={styles.newSectionEntryImage}
+                            />
                             <h3 className={styles.newSectionEntryTitle}>Kommuniziert gemeinsam via Chat</h3>
                             <p className={styles.newSectionEntryDescription}>
                                 Sobald dein ausgewählter Koch die Anfrage bestätigt, können die letzten Einzelheiten via Chat gemeinsam
@@ -226,7 +241,7 @@ export function HomePage({ signedInUser, searchParameters, heroCooks, heroMenus 
                             </div>
                             <PEButton title="Koch finden" onClick={(): void => undefined} />
                         </div>
-                        <Image src="/home/3/dinner.jpeg" alt="" width={500} height={500} className={styles.section3BodyImage} />
+                        <Image unoptimized src="/home/3/dinner.jpeg" alt="" width={500} height={500} className={styles.section3BodyImage} />
                     </div>
                 </div>
 
